360270df9c629d630c149c2e3736ceea84ac8e01
Craig Iturbe <citurbe@gmail.com>
2016-08-14 18:34:04 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+deli = []
+line_length = 1
+
+def line(deli)
+if deli == []
+  puts "The line is currently empty."
+else
+  output = "The line is currently:"
+  number = 1
+  deli.each do |name|
+    output << " #{number}. #{name}"
+    number += 1
+  end
+  puts output
+end
+end
+
+def take_a_number (deli, name)
+  deli << name
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
+  return deli
+end
+
+def now_serving (deli)
+if deli == []
+  puts "There is nobody waiting to be served!"
+else
+  puts "Currently serving #{deli.shift}."
+
+end
+return deli
+end
