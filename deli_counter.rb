f8e8be9de7920bc211a8a61a729ab275c32d6a08
krissasantos <krissa.santos31@gmail.com>
2016-08-30 02:33:36 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,34 +1,30 @@
 katz_deli = []
 
 def line(queue)
-  string = "The line is currently:" 
+  string = "The line is currently:"
 
   if queue.empty?
     puts "The line is currently empty."
   else
-
-    queue.each_with_index do |name, idx|
-      string << " #{idx + 1}. #{name}"
-    end
+    queue.each_with_index {|name, idx| string << " #{idx + 1}. #{name}" }
     puts string
   end
 end
 
+
 def take_a_number(queue, name)
-
-    queue << name
-    puts "Welcome, #{name}. You are number #{queue.index(name) + 1} in line."
-
+  queue << name
+  puts "Welcome, #{name}. You are number #{queue.index(name) + 1} in line."
 end
 
 
 
-def now_serving(now_serving_line)
-  if now_serving_line.empty?
+def now_serving(queue)
+  if queue.empty?
     puts "There is nobody waiting to be served!"
   else
-    first_in_line = now_serving_line.shift
-    puts "Currently serving #{first_in_line}."
+    first_person = queue.shift
+    puts "Currently serving #{first_person}."
   end
-end
 
+end
