d4601f573d084a1151dcdbbe74c930b7d6a343e4
Stephen Lin <slin7219@bths.edu>
2016-09-05 23:19:12 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def line(arr)
+  if arr.empty?
+    puts "The line is currently empty."
+  else
+    str = "The line is currently:"
+    for count in 1..arr.length
+      str << " #{count}. #{arr[count-1]}"
+    end
+    puts str
+  end
+end
+
+def take_a_number(arr, str)
+  arr << str
+  puts "Welcome, #{str}. You are number #{arr.length} in line."
+end
+
+def now_serving(arr)
+  if arr.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{arr[0]}."
+    arr.shift()
+  end
+end
