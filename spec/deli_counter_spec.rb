3b45aa4b7e96d2c411595daff1757110c1ba664d
Noah Schechter <noahschechter7@gmail.com>
2016-09-19 21:57:57 +0000

    Done.

--- spec/deli_counter_spec.rb
+++ spec/deli_counter_spec.rb
@@ -68,3 +68,32 @@ describe 'Deli Counter' do
   end
 
 end
+
+katz_deli = []
+def line(katz_deli)
+  if katz_deli.count == 0
+    puts "The line is currently empty."
+  else
+    string = ""
+    i = 0
+    while i < katz_deli.length
+      string = string + " #{i+1}. #{katz_deli[i]}"
+      i = i + 1
+    end
+    puts "The line is currently:" + string
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.count == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+  end
+end
