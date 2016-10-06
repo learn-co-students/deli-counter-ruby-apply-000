a579cdd1b5cdbc660fa17359dc0e83c493152778
Karl Ingram <kingram100@yahoo.com>
2016-10-06 15:09:10 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def line(linearray)
+  if linearray.length == 0
+    puts "The line is currently empty."
+  else
+    idx = 1
+    linestring = "The line is currently: "
+    linearray.each do |entry|
+      linestring += "#{idx.to_s}. #{entry} "
+      idx += 1
+    end
+    linestring[linestring.length - 1] = ""
+    puts linestring
+  end
+end
+
+def take_a_number(line,name)
+  line.push(name)
+  puts "Welcome, #{name}. You are number #{line.length} in line."
+end
+
+def now_serving(line)
+  if line.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{line.shift}."
+  end
+end
