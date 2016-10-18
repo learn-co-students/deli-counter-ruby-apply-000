cdce8120788ed7aa2ced4e3cc81cc4c256b53a9a
frank warren <frank.warren@gmail.com>
2016-10-18 17:22:35 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+
+def line(katz_deli)
+if katz_deli.length == 0
+  puts "The line is currently empty."
+else
+  place_in_line="The line is currently:"
+  katz_deli.each_with_index do |name, index|
+    place_in_line += " #{index.to_i+1}. #{name}"
+  end
+  puts "#{place_in_line}"
+end
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length==0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.shift}."
+  end
+end
+
+def take_a_number(katz_deli,name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
