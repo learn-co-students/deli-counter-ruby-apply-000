75a1f964b20194306181906165f08b79e7c176f0
Karl Ingram <kingram100@yahoo.com>
2016-10-19 20:11:02 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,5 @@
 def line(line_array)
-  if line_array.length == 0
+  if line_array.empty?
     puts "The line is currently empty."
   else
     idx = 1
@@ -13,10 +13,10 @@ def line(line_array)
 end
 
 def take_a_number(line_array, name)
-  line_array.push(name)
+  line_array << name
   puts "Welcome, #{name}. You are number #{line_array.length} in line."
 end
 
 def now_serving(line_array)
-  line_array.length == 0 ? (puts "There is nobody waiting to be served!") : (puts "Currently serving #{line_array.shift}.")
+  line_array.empty? ? (puts "There is nobody waiting to be served!") : (puts "Currently serving #{line_array.shift}.")
 end
