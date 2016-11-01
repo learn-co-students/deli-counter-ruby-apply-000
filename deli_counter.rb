26abc8e0714615b2808f8d72a67df1fc79dfc1a5
frank warren <frank.warren@gmail.com>
2016-11-01 15:39:45 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,12 +4,11 @@ def line(katz_deli)
 if katz_deli.length == 0
   puts "The line is currently empty."
 else
-  place_in_line="The line is currently:"
-  katz_deli.each_with_index do |name, index|
-    place_in_line += " #{index.to_i+1}. #{name}"
-  end
-  puts "#{place_in_line}"
+  message= "The line is currently:"
+  katz_deli.each_with_index do |name, index| message  += " #{index.to_i+1}. #{name}"
 end
+puts "#{message}"
+  end
 end
 
 def now_serving(katz_deli)
