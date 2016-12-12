e0df3200c4be84dd2b3255b1a8bec5f3e9b251af
Daniel Lempesis <danlrcn@hotmail.com>
2016-12-12 00:17:47 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,8 @@
-katz_deli = []
-
 def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
   else
   puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
-
 end
 
 
@@ -22,8 +19,7 @@ end
 def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
-
-  elsif katz_deli.length > 0
+  else
     puts "Currently serving #{katz_deli.shift()}."
 end
 end
