28de7886033fd52a2a01807533bdbdd95ad52158
Nick <anaya.nicholas@gmail.com>
2016-12-14 15:55:53 -0500

    Update deli_counter.rb

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,15 +11,21 @@ def line(line_array)
   end
 end
 
-def take_a_number(katz_deli, name)
-  katz_deli << name
+idx = 0 
 
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+def take_a_number(array)
+  if array.length == 0
+    puts "Welcome customer. You are the first person in ine!"
+  else
+    idx += 1
+    array << idx
+    puts "Welcome customer. You are number #{array.length} in line."
+  end
 end
 
 def now_serving(katz_deli)
-  if katz_deli.length > 0
-    puts "Currently serving #{katz_deli.shift}."
+  if katz_deli != 0
+    puts "Currently serving number #{array.unshift}."
   else
     puts "There is nobody waiting to be served!"
   end
