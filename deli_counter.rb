b56241c4ca9c5221ec0669483bc9510415411b81
Carlos  Grijalva <grijalva.carlos@gmail.com>
2016-12-20 20:25:32 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli << name
+  katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
