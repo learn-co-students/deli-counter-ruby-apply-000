c725440b45477ff39b7121bf23f686e7e11b3027
Zach Dubno <zachedubno@gmail.com>
2017-01-05 00:01:04 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def line(line)
+  banana = []
+  if line.length == 0
+  n = 1
+    puts "The line is currently empty."
+  else
+    line.each do |item|
+      banana << "#{n}. #{item} "
+    end
+    puts "The line is currently: #{banana}"
+  end
+end
+
+def take_a_number()
+
+end
+
+def now_serving()
+
+end
