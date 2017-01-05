1acd6a5d49585b4f90c693fa352c1c5f12572c72
Zach Dubno <zachedubno@gmail.com>
2017-01-05 00:12:48 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,20 +1,27 @@
-def line(line)
-  banana = []
-  if line.length == 0
+def line(array)
+  banana = ""
   n = 1
+  if array.length == 0
     puts "The line is currently empty."
   else
-    line.each do |item|
-      banana << "#{n}. #{item} "
+    array.each do |item|
+      banana << " #{n}. #{item}"
+      n += 1
     end
-    puts "The line is currently: #{banana}"
+    puts "The line is currently:#{banana}"
   end
 end
 
-def take_a_number()
-
+def take_a_number(array, name)
+  array << name
+  puts "Welcome, #{name}. You are number #{array.length} in line."
 end
 
-def now_serving()
-
+def now_serving(array)
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}."
+    array.shift
+  end
 end
