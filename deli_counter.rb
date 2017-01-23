eb808da731941e78e8d56b92f699fd1799433ecc
Edwin Goo <edwin_goo@yahoo.com>
2017-01-23 06:11:22 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  i = 0
+  lst = []
+  while i < array.length
+    lst.push((i + 1).to_s + ". " + array[i])
+    i += 1
+  end
+  if array.length > 0
+    puts "The line is currently: #{lst.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+def now_serving(array)
+  if array.length > 0
+    puts "Currently serving #{array[0]}."
+    array.shift()
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
