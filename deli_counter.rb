63295c09a4ccc3ee758d5cf76082f156b8fd89e9
Wellington Vicioso <wellington.vicioso@gmail.com>
2017-02-28 18:18:36 -0500

    done

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ def take_a_number(arr, person)
 end
 
 def now_serving(arr)
-  if arr.length == 0 ?
+  if arr.length == 0
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{arr.shift}."
