7473e2fc8462bfd40ae0eb69a33e42cbbff54c1c
Andrew Zheng <andrew.zheng725@gmail.com>
2017-03-01 16:25:26 +0000

    changed each_index to each_with_index iteration and returned push result

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,15 +4,14 @@ def line(deli_array)
     puts "The line is currently empty."
   else
     line_string = "The line is currently:"
-    deli_array.each_index {|index| line_string += " #{index + 1}. #{deli_array[index]}" }
+    deli_array.each_with_index {|val, index| line_string += " #{index + 1}. #{val}" }
     puts line_string
   end
 end
 
 def take_a_number(deli_array, new_customer)
+  puts "Welcome, #{new_customer}. You are number #{deli_array.length + 1} in line."
   deli_array.push(new_customer)
-  puts "Welcome, #{new_customer}. You are number #{deli_array.length} in line."
-  deli_array
 end
 
 def now_serving(deli_array)
@@ -22,4 +21,3 @@ def now_serving(deli_array)
     puts "Currently serving #{deli_array.shift}."
   end
 end
-
