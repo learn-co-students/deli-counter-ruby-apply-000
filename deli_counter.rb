c65e4a03c0097a081f18fa7476b92e1d89210beb
Hyewon Karina Yang <hyewon.karina.yang@gmail.com>
2017-03-20 15:59:59 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,41 @@
-# Write your code here.
+def line(array)
+
+  message = "The line is currently:"
+
+  if array.length == 0
+    message = "The line is currently empty."
+  elsif
+    array.each_with_index do |el, idx|
+      message << " " << (idx+1).to_s << ". " << el
+    end
+  end
+  puts message
+
+end
+
+
+
+def take_a_number(katz_deli, name)
+
+  katz_deli << name
+  p katz_deli
+
+  katz_deli.each_with_index do |el, idx|
+    if name == el
+      puts "Welcome, " << name << ". " << "You are number " << (idx+1).to_s << " in line."
+    end
+  end
+
+end
+
+def now_serving(array)
+
+  message = "Currently serving "
+  if array == []
+    message = "There is nobody waiting to be served!"
+  elsif
+    message << array[0]  << "."
+    array.shift
+  end
+  puts message
+end
