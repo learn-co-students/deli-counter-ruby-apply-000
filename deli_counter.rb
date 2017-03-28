24ad126e4bc3c155db39d2d1539bb0008f960534
David Langva <davidlangva@gmail.com>
2017-03-28 21:18:39 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+str = "The line is currently: "
+# if the line is not empty, add each name to the result string and include their human position in line
+if katz_deli.empty?
+  puts "The line is currently empty."
+  return "The line is currently empty."
+else
+
+  katz_deli.each_with_index do |name, position|
+    str = str + "#{position + 1}. #{name} "
+  end
+  puts str.strip
+  #return str.strip
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts ("Welcome, #{name}. You are number #{katz_deli.length} in line.")
+end
+
+def now_serving(katz_deli)
+  unless katz_deli.empty?
+    puts "Currently serving #{katz_deli.delete_at(0)}."
+  else puts "There is nobody waiting to be served!"
+  end
+end
