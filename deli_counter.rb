90568dc765f7e696f75509a122495591520d4336
Daniel Conway <danconway14@yahoo.com>
2017-04-24 16:20:24 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,34 @@
-# Write your code here.
+
+def line(katz_deli)
+  if katz_deli.length > 0
+    puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
+  elsif katz_deli.length == 0
+    puts "The line is currently empty."
+  else puts nil
+  end
+end
+
+def take_a_number(other_deli, name)
+  if other_deli.length > 0
+    other_deli.push(name)
+  puts "Welcome, #{name}. You are number 4 in line."
+elsif other_deli.length == 0
+  other_deli.push(name)
+  puts "Welcome, #{name}. You are number 1 in line."
+else
+  puts nil
+end
+end
+
+
+
+def now_serving(name)
+  if name.length > 0
+    name.shift
+    puts "Currently serving Logan."
+  elsif name.length == 0
+  puts "There is nobody waiting to be served!"
+else
+  puts nil
+end
+end
