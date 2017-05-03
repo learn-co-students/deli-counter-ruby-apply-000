2030e7a316d0b7bd9feb7aef3e61317c62fed891
Yang Lu <young.lu1995@gmail.com>
2017-05-03 11:30:18 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,6 @@
 def line(linearr)
+  #Prints out the entire line with position and name
+  #if line empty, says so
   if linearr.length==0
     puts "The line is currently empty."
   else
@@ -12,11 +14,15 @@ end
 
 
 def take_a_number(linearr, name)
+  # adds person with name to back of linearr
+  # tells person their position in line
   linearr<<name
   puts "Welcome, #{name}. You are number #{linearr.length} in line."
 end
 
 def now_serving(linearr)
+  #calls next person in line and removes them from linearr
+  # if line empty, prints that nobody is waiting
   if linearr.length==0
     puts "There is nobody waiting to be served!"
   else
