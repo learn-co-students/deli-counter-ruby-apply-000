2d5a6aaa15515ed28594417c11b760c3cb88e247
Jevaughn McKenzie <jmckenzie1004@gmail.com>
2017-05-15 16:44:37 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,30 +2,29 @@
 
 katz_deli = []
 
-def line(line_of_people)
-  if line_of_people.empty?
+def line(deli_line)
+
+  if deli_line.empty?
     puts "The line is currently empty."
   else
-    roll_call = "The line is currently:"
-    i = 0
-    while i < line_of_people.length
-      roll_call << " #{i + 1}. #{line_of_people[i]}"
-      i += 1
+    current_line = "The line is currently:"
+    deli_line.map do |name|
+      current_line += " #{deli_line.index(name) + 1}. #{name}"
     end
-    puts roll_call
+    puts current_line
   end
 end
 
-def take_a_number(line_of_people, person_name)
-  line_of_people << person_name
-  puts "Welcome, #{person_name}. You are number #{line_of_people.index(person_name) + 1} in line."
+def take_a_number(deli_line, name)
+  deli_line << name
+  puts "Welcome, #{name}. You are number #{deli_line.index(name) + 1} in line."
 end
 
-def now_serving(line_of_people)
-  unless line_of_people.empty?
-    puts "Currently serving #{line_of_people.first}."
-    line_of_people.shift
-  else
+def now_serving(deli_line)
+  if deli_line.empty?
     puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli_line.first}."
+    deli_line.shift
   end
 end
