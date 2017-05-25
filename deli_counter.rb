65dd736bbb54bbc6480dfe95e0958f5dfbc89556
Kira R <kiramrice@gmail.com>
2017-05-25 16:48:08 -0400

    Update deli_counter.rb

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,5 @@
-katz_deli = [] #create_an_empty_array
-def line(katz_deli) #descri 'line' and calling an argument/my_array
+katz_deli = [] 
+def line(katz_deli) 
   if katz_deli == [] #must '==' for conditional, if 'katz_deli == empty_array then nobody in line'
     puts "The line is currently empty."
   else
@@ -8,20 +8,17 @@ def line(katz_deli) #descri 'line' and calling an argument/my_array
 end
 
 katz_deli = [] #array from above
-def take_a_number(katz_deli, name) #need two arguments but why? B/c adding someone into line?
-  katz_deli << name #adding Ada to line/string, '<<' is 'shovel_method which allows to add to end of array'
-  puts "Welcome, #{name}. You are number #{katz_deli.size} in line." #I'm not entirely sure how to 'access_by_index' in this case
+def take_a_number(katz_deli, name) 
+  katz_deli << name #adding Ada to line/string, '<<' is 'shovel_method which allows you to add to end of array'
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
 def now_serving(katz_deli)
-  if katz_deli != [] #i('!=' from Codecademy), if not equal empty_array then currently serving
+  if katz_deli != [] #if not equal empty_array then currently serving
   puts "Currently serving #{katz_deli[0]}." #serving first person in line
   katz_deli.shift #should add person to 'end' of line, .shift because serving 1st customer but then 'removing' customer and shifting next customers up
 else
   puts "There is nobody waiting to be served!" #'else' less than 1 then serving no one
-  end #double ends to end if/else conditional
+  end
 end
 
-#this one was tough. I had too much code at first and my def method arguments were out of order (or does the order matter?)
-#utilized Stackoverflow, Codecademy, Github, Intro_to_Ruby
-#understood to use string_interpolation, methods, arguments, array_basics, if/else/(elseif) conditionals
