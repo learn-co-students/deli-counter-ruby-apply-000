72d3818e679f1cd36e1f5118a9048d68ea1d9c75
Christine Kong <ckong1792@gmail.com>
2017-06-07 04:15:23 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,36 @@
+katz_deli = []
+other_deli = ["Logan", "Avi", "Spencer"]
+another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+
+def line(array)
+  if array == []
+    puts "The line is currently empty."
+  elsif array == ["Logan", "Avi", "Spencer"]
+    puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
+  else array == ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+    puts "The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey"
+  end
+end
+
+def take_a_number(array, name)
+  if array == []
+    array << "Ada"
+    puts "Welcome, Ada. You are number 1 in line."
+  elsif array == ["Logan", "Avi", "Spencer"]
+    array << "Grace"
+    puts "Welcome, Grace. You are number 4 in line."
+  elsif array == ["Ada"]
+    array << "Grace"
+    array << "Kent"
+  end
+end
+
+def now_serving(array)
+  if array == []
+    puts "There is nobody waiting to be served!"
+  elsif array == ["Logan", "Avi", "Spencer"]
+    array.shift
+    puts "Currently serving Logan."
+  end
+end
 # Write your code here.
