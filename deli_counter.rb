881de8a8ae86712d52363737f881fdc5d566976f
Kevin Wu <kevinwutech@gmail.com>
2017-06-09 01:00:41 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def del_line(array)
+  katz_deli = []
+if array.length < 1
+  return "The line is currently empty."
+else
+  array.each_with_index do |el, idx|
+    katz_deli <<  "#{idx}. #{el}"
+  end
+end
+katz_deli
+end
