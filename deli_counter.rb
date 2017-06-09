74f824c51976b18253d79520d2df713e27a07c27
Christine Kong <ckong1792@gmail.com>
2017-06-09 00:30:46 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,36 +1,35 @@
-katz_deli = []
-other_deli = ["Logan", "Avi", "Spencer"]
-another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
-
 def line(array)
   if array == []
     puts "The line is currently empty."
-  elsif array == ["Logan", "Avi", "Spencer"]
-    puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
-  else array == ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
-    puts "The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey"
+  else
+    counter = 0
+    until counter == array.size
+      string = "The line is currently:"
+      array.each_with_index do |name, index|
+        string << " #{index +1}. #{name}"
+        counter += 1
+      end
+    end
+ puts  string
   end
 end
 
 def take_a_number(array, name)
   if array == []
-    array << "Ada"
-    puts "Welcome, Ada. You are number 1 in line."
-  elsif array == ["Logan", "Avi", "Spencer"]
-    array << "Grace"
-    puts "Welcome, Grace. You are number 4 in line."
-  elsif array == ["Ada"]
-    array << "Grace"
-    array << "Kent"
+    array << "#{name}"
+    puts "Welcome, #{name}. You are number 1 in line."
+  else
+    z = array.size
+    array << "#{name}"
+    puts "Welcome, #{name}. You are number #{z +1} in line."
   end
 end
 
 def now_serving(array)
   if array == []
     puts "There is nobody waiting to be served!"
-  else array == ["Logan", "Avi", "Spencer"]
-    array.shift
-    puts "Currently serving Logan."
+  else puts "Currently serving #{array[0]}."
+      array.shift
   end
 end
 # Write your code here.
