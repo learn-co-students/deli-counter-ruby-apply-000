e22c894ddc3d47e835c61667a32e5ae3db67bdad
Alexander Podolsky <alexander_podolsky@alumni.brown.edu>
2017-06-18 04:35:46 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+
+katz_deli = []
+#was I missing something about there needing to be an array insantiated
+def line(x)
+  line_array = []
+  if x.empty?
+    puts "The line is currently empty."
+  else
+    x.each.with_index(1)  { |name, index|
+      line_array.push("#{index}. #{name}")}
+    puts "The line is currently: #{line_array.join(" ")}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+    katz_deli.push(name)
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+
+def now_serving(array)
+  if array.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}."
+    array.shift
+  end
+end
