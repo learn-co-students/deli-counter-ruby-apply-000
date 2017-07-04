43f58690fc29fa30ddb9340d885c16ae818c6dd7
Alexander Marks-Katz Marks-Katz <english3000.org@gmail.com>
2017-07-04 16:15:28 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    i = 0
+    linePrint = "The line is currently:"
+    until i == array.length
+      y = i + 1
+      linePrint += " #{y}. #{array[i]}"
+      i += 1
+    end
+    puts linePrint
+  end
+end
+
+def take_a_number(queue, string)
+  place = queue.length + 1
+  queue << string
+  puts "Welcome, #{string}. You are number #{place} in line."
+end
+
+def now_serving(arr)
+  if arr[0] == nil
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{arr[0]}."
+    arr.shift
+  end
+end
