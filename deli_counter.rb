c3adae622cae51562b3f42944e7d62aab8c10104
Ramon Cabral <rrcm16@gmail.com>
2017-07-12 17:20:00 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli[0]}
+    puts "Currently serving #{katz_deli[0]}."
     katz_deli.shift()
   end
 end
