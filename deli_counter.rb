59e08a25b2957f50daada4d42a9b2bd8305724f4
Joshua Denenberg <j.denenberg42@gmail.com>
2017-08-09 23:45:06 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def line(line_array="empty")
+  if line_array=="empty"
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(line_array, new_customer)
+end
+
+
+def now_serving(line_array='empty')
+  if line_array=="empty"
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Now serving #{line_array[0]}"
+    line_array.shift
+  end
+end
