d4ee14331cd6345405a40c7c35642f1a8f705f9a
Diana Shin <diana.shin@nyu.edu>
2017-08-10 20:53:01 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,14 +14,15 @@ def now_serving(katz_deli)
 end
 
 def line(katz_deli)
-    if katz_deli.length == 0
-      puts "The line is currently empty."
-    else
-      message="The line is currently: "
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    message="The line is currently:"
 
-      katz_deli.each_with_index do |value, index|
-        message += " #{index +1}. #{value}"
-      end
-       puts message
-     end
+    katz_deli.each_with_index do |value, index|
+      message += " #{index + 1}. #{value}"
+    end
+
+    puts message
+  end
 end
