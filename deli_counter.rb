72bce8ed574c37574ee5fa00b12dadd0ec4532c8
Will-Will <wkboss@gmail.com>
2017-08-17 12:51:27 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,22 +1,21 @@
-#currently debugging
-
-#note: not using //else nil//
-
 katz_deli = []
 
 ##### Method 1: SHOWS LINE STATUS
 #note: use if/else, case used for large computations
-def line (katz_deli)
-  if katz_deli.length == 0
+#note: need to set default argument, possibly //def line (d = katz_del)//
+# and would need to change all katz_del to d
+def line (katz_del)
+  if katz_del.length == 0
     puts "The line is currently empty."
   #note: IF FAILS try //elsif katz_deli.length >= 1//
   #note: replaced //loop// with //while//
   else
     i = 0
-    while i < katz_deli.length
+    while i < katz_del.length
     #note: IF FAILS try //katz_del[0] + 1]//
     puts "The line is currently: #{katz_del.index[i + 1]}. #{katz_del[i]}"
     i += 1
+    end
   end
 end
 
@@ -47,66 +46,3 @@ def now_serving (katz_deli)
     katz_deli.shift
   end
 end
-
-
-
-
-#########
-#########
-
-=begin
-take_a_number(katz_deli, "Ada")
-#=> Welcome, Ada. You are number 1 in line.
-
-take_a_number(katz_deli, "Grace")
-#=> Welcome, Grace. You are number 2 in line.
-take_a_number(katz_deli, "Kent")
-#=> Welcome, Kent. You are number 3 in line.
-
-line(katz_deli)
-#=> "The line is currently: 1. Ada 2. Grace 3. Kent"
-
-
-now_serving(katz_deli)
-#=> "Currently serving Ada."
-
-
-line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent"
-
-
-take_a_number(katz_deli, "Matz")
-#=> Welcome, Matz. You are number 3 in line.
-
-
-line(katz_deli)
-#=> "The line is currently: 1. Grace 2. Kent 3. Matz"
-
-
-now_serving(katz_deli)
-#=> "Currently serving Grace."
-
-
-line(katz_deli)
-#=> "The line is currently: 1. Kent 2. Matz"
-
-1)
-Build the line method that shows everyone their current place in the line.
-If there is nobody in line, it should say "The line is currently empty.".
-2)
-Build a method that a new customer will use when entering the deli.
-The take_a_number method should accept two arguments, the array for the
-current line of people (katz_deli), and a string containing the name of
-the person wishing to join the line. The method should return the person's
-name along with their position in line. Top-Tip: Remember that people
-like to count from 1, not from 0 ("zero") like computers.
-3)
-Build the now_serving method which should call out (i.e. puts) the next
-person in line and then remove them from the front. If there is nobody
-in line, it should call out (puts) that "There is nobody waiting to be
- served!".
-
- Hint: Review adding and removing elements from an array as well as iterating
-  with index numbers. Also, many of the methods to add and remove elements
-  from an array can also be used to add and remove elements to a string. This
-   will help you solve the lab.
-=end
