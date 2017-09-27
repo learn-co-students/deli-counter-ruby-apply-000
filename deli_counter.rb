cc223d824960e955e649b35d1554fb4e907ba668
Kartik Parihar <kartikvips@gmail.com>
2017-09-27 14:17:16 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,12 +14,8 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  if katz_deli.length==0
-    puts "Welcome, #{name}. You are number 1 in line."
-  else
-    puts "Welcome, #{name}. You are number #{katz_deli.length+1} in line."
-  end
-  katz_deli.push(name)
+  katz_deli<<name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
