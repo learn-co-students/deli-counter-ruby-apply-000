a102e521bd54125f31cdb29cc5b983487056bae8
noletubby77 <noletubby77@hotmail.com>
2017-10-02 20:01:28 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,26 +1,29 @@
 # Write your code here.
-def line(katz_deli)
-  if katz_deli.length == 0
+def line(deli_counter)
+  if deli_counter.length == 0
     puts "The line is currently empty."
   else
     line_array = []
-    katz_deli.each_with_index do |name, position|
-      line_array.push ("#{position + 1}. #{name}")
+    deli_counter.each_with_index do |name, i|
+      line_array.push ("#{i + 1}. #{name}")
     end
     puts "The line is currently: #{line_array.join(" ")}"
   end
 end
 
-def take_a_number(katz_deli, customer)
-  katz_deli.push(customer)
-  puts "Welcome, #{customer}. You are number #{katz_deli.length} in line."
+def take_a_number(deli_counter, customer)
+  deli_counter.push(customer)
+  puts "Welcome, #{customer}. You are number #{deli_counter.length} in line."
 end
 
-def now_serving(katz_deli)
-  if katz_deli.length == 0
+def now_serving(deli_counter)
+  if deli_counter.length == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli[0]}."
-    katz_deli.shift
+    puts "Currently serving #{deli_counter[0]}."
+    deli_counter.shift
   end
 end
+line([])
+now_serving(["James","Lena","Rio"])
+take_a_number(["James"], "Mike")
