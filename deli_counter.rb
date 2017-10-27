2b0a97c0d28c5e3500f34c3fe80dcb0ccb39d870
David Y. Hall <me@davidyhall.com>
2017-10-27 18:15:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,17 @@
 def line(names)
-currentline = "The line is currently:"
   if names.length == 0
     puts "The line is currently empty."
   else
+  currentline = "The line is currently:"
     for i in 0...names.length
       currentline << " #{i+1}. #{names[i]}"
     end
   puts currentline
   end
 end
+
+def take_a_number(katz_deli, n)
+  names << n
+  puts "Welcome, #{n}. You are number #{names.length} in line."
+end
+
