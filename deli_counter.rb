5a016324d53e94ef9166d47e72d325b47ffc7e26
David Y. Hall <me@davidyhall.com>
2017-10-27 17:56:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,5 @@
 def line(names)
-  return "The line is currently empty." if names.length == nil
+  return "The line is currently empty." if names.length == 0
   print "The line is crrently: "
   for i in 0...names.length
     print "#{i+1}. #{names[i]}. "
