16f79cfc04e5eb88f67d2d872dcaf59b2ce8df98
Cristy Lucke <cristy.lucke@gmail.com>
2017-11-06 21:00:42 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,4 @@
+## Outputs line if populated, otherwise outputs empty message
 def line(line)
   if (line.empty?)
     puts "The line is currently empty."
@@ -10,11 +11,15 @@ def line(line)
   end
 end
 
+## Adds new person to line array
+## Outputs welcome message including name and place in line
 def take_a_number(line, name)
   line << name
   puts "Welcome, #{name}. You are number #{line.size} in line."
 end
 
+## If line is populated, removes next person and outputs currently serving message
+## Outputs empty message if line is empty
 def now_serving(line)
   if line.any?
     puts "Currently serving #{line.shift}."
