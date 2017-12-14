cc7236ecd77687f9862675a13827aedfde012f2f
Abigail Swarth <abigailswarth@gmail.com>
2017-12-14 20:05:47 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def line(katz_deli)
+  num_in_line = "The line is currently:"
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    counter = 1
+    katz_deli.each do |name|
+      num_in_line << " #{counter}. #{name}"
+      counter += 1
+    end
+    #katz_deli.each_with_index do |name, index|
+    #  num_in_line << " #{index + 1}. #{name}"
+    #end
+    puts num_in_line
+  end
+end
+
+def take_a_number(katz_deli, person)
+  katz_deli << person
+  puts "Welcome, #{person}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.shift}."
+  end
+end
--- spec/deli_counter_spec.rb
+++ spec/deli_counter_spec.rb
@@ -22,7 +22,7 @@ describe 'Deli Counter' do
 
       it 'works with different people in line' do
 	      expect($stdout).to receive(:puts).with("The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey")
-	line(another_deli)
+	      line(another_deli)
       end
     end
   end
