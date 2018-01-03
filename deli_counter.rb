a6b50e1a25dd338d81378de879016a40b6cf785a
Barney Fabian <bafa@tutanota.de>
2018-01-03 20:56:45 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,10 @@
 katz_deli = []
 
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
 def line(katz_deli)
   current = "The line is currently:"
   if katz_deli.length != 0
@@ -11,11 +16,6 @@ def line(katz_deli)
   end
 end
 
-def take_a_number(katz_deli, name)
-  katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-end
-
 def now_serving(katz_deli)
   if katz_deli.length != 0
     puts "Currently serving #{katz_deli.shift}."
@@ -23,6 +23,3 @@ def now_serving(katz_deli)
     puts "There is nobody waiting to be served!"
   end
 end
-
-    
-    
