3780685e24f21a4a201895ac803b23195b05ab8c
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2018-01-18 18:41:14 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+def line(array)
+  if array.length > 0
+    size = 0
+    str = ""
+    while(size < array.length)
+      str += " #{size +1}. #{array[size]}"
+      size += 1
+    end
+    puts "The line is currently:" + str
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(array, name)
+  puts "Welcome, #{name}. You are number #{array.length + 1} in line."
+  array << name
+end
+
+def now_serving(array)
+  if array.length > 0
+      puts "Currently serving #{array.shift()}."
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
