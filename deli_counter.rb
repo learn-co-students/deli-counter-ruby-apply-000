3b37e7f2a2a405f06388b00740d9ea840ebe1bc0
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2018-01-22 00:49:21 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,31 @@
 # Write your code here.
+
 def line(array)
   if array.length > 0
-    size = 0
     str = ""
-    while(size < array.length)
-      str += " #{size +1}. #{array[size]}"
-      size += 1
+    array.each do |person|
+      str += " #{array.index(person) + 1}. #{person}"
     end
     puts "The line is currently:" + str
   else
     puts "The line is currently empty."
   end
 end
+
+# ALTERNATIVE LINE FUNCTION:
+# def line(array)
+#   if array.length > 0
+#     size = 0
+#     str = ""
+#     while(size < array.length)
+#       str += " #{size + 1}. #{array[size]}"
+#       size += 1
+#     end
+#     puts "The line is currently:" + str
+#   else
+#     puts "The line is currently empty."
+#   end
+# end
 
 def take_a_number(array, name)
   puts "Welcome, #{name}. You are number #{array.length + 1} in line."
