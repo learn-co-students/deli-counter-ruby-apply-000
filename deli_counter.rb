a8862fe3ee4ce9dbda014711e1072a51af001e15
Shahrukh Jalil <jalilshahrukh@gmail.com>
2018-02-16 15:19:04 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,14 +2,10 @@
 def line(katz_deli)
   if katz_deli.empty? 
     puts "The line is currently empty." 
-  else 
-    print "The line is currently: "
+  else
     index = 0
-    katz_deli.each do |name| 
-      print index+=1 
-      print ". "
-      print name
-      print " "
-    end  
+    the_line = "The line is currently: "
+    katz_deli.each {|name| the_line += "#{index+=1}. #{name} "} 
+    puts the_line.strip!
   end    
 end 
