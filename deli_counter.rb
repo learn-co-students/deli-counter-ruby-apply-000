28ee8827fbef692fdfd36b5f3c255a46dbf87fe7
dzeit216@gmail.com Zeit <dzeit216@gmail.com>
2018-03-28 19:48:43 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+# Write your code here 
+def line(names)
+  if names.length == 0 
+    puts "The line is currently empty."
+  else 
+    current_line = "The line is currently:"
+    names.each.with_index(1) do |name, i|
+      current_line << " #{i}. #{name}"
+    end 
+    puts current_line
+  end
+end 
+
+def take_a_number(current_line, name)
+  current_line.push(name)
+  puts "Welcome, #{name}. You are number #{current_line.length} in line."
+end 
+
+def now_serving(current_line)
+  if current_line.length == 0 
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{current_line[0]}."
+    current_line.shift
+  end 
+end 
+  
