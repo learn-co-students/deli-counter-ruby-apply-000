8d19c9539f125ad680c35198a07ad4916a0b104d
iwilliams83 <indirajamurath@gmail.com>
2018-04-02 01:30:40 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def line(deli)
+  if deli.length == 0
+    puts "The line is currently empty."
+  else
+    s = "The line is currently:"
+    x = 1
+    for i in deli
+      s += " #{x}. #{i}"
+      x += 1
+    end
+    puts s
+  end
+end
+
+def take_a_number(deli, person) #q is the array, and s is the person's name as a string
+      deli << person
+      n = deli.length
+      puts "Welcome, #{person}. You are number #{n} in line."
+  end
+end
+
+def now_serving(arr)
+    if arr.length == 0
+     puts "There is nobody waiting to be served!"
+    else
+     puts "Currently serving #{arr[0]}."
+     arr.shift
+    end
+end
