3d758df0160cbc18d7cd80839e3a4e5d9de16e1a
Malcolm Leake <echelon1089@gmail.com>
2018-04-14 23:54:17 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+
+def line(katz_deli)
+  if deli.empty?
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    katz_deli.each.with_index(1) do |name, i|
+      current_line << " #{i}. #{name}"
+    end
+    puts current_line
+  end
+end
+
+
+
+def take_a_number(katz_deli, name_of_person)
+  katz_deli.each.with_index(1) do |name, i|
+  puts "Welcome, #{name}. You are number #{i} in line."
+end
+end
+
+
+
+
+def now_serving(katz_deli)
+  puts "Currently serving."
+end
