60bfbc0b863e76bf59362f9833281f10f774fac3
kai.han@balliol.ox.ac.uk <kai.han@balliol.ox.ac.uk>
2018-04-25 14:45:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(array)
+  if array.empty? 
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: #{array.each { |i| puts "#{i + 1}. #{array[i]}}}"
+    array.each { |i| puts 
