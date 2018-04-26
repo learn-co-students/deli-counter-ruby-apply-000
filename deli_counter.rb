3153d3f6cc27b07c7e683322826031c783dd5fd7
Tony Ho <ho.tonym@gmail.com>
2018-04-26 16:13:03 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+katz_deli = []
+other_deli = ["Logan", "Avi", "Spencer"]
+another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+
+def line(array)
+
+if array != "" || array != nil
+  array
+end
+
+end
+
+def take_a_number
+
+end
