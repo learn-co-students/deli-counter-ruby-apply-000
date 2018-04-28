1f50521a66c5adb50244d1b902a67780abbb02a6
richardojo04@gmail.com <richardojo04@gmail.com>
2018-04-28 14:00:17 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+require "pry"
+katz_deli = []
+def line(katz_deli)
+  if katz_deli.empty?
+	puts "The line is currently empty."
+  else
+	increment_count = 1
+	lineNamesandNumbers = []
+	katz_deli.each do |name|
+	  lineNamesandNumbers.push("#{increment_count}. #{name}")
+	  increment_count += 1
+	end
+	puts "The line is currently: #{lineNamesandNumbers.join(" ")}"
+  end
+end
+
+def now_serving(line)
+  if line.empty?
+	puts "There is nobody waiting to be served!"
+  else
+	puts "Currently serving #{line.shift}."
+  end
+end
+
+def take_a_number(line, name)
+  line.push(name)
+  puts "Welcome, #{name}. You are number #{line.length} in line."
+end
