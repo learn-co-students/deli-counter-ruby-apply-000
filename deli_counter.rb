c727b933693d6d4b313df16136158469ae85d9f8
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-06-04 03:57:54 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,9 @@ katz_deli = [ ]
 
 
 def line
-  #use .each for this, Omar.
+  #use .each for this, Omar.  And need to somehow match each name with its index number + 1, for puts; maybe return a new_array listing, for example: "1. Ada.  2. Grace..."
   if
-    puts "#{katz_deli[ ]} is currently number #{katz_deli[ ] + 1}" in line.
+    puts "The line is currently: #{new_array}." # Note that the example sentence they provide does not end with  a sentence.
   else
     puts "The line is currently empty."
 end
@@ -19,9 +19,9 @@ end
 
 def now_serving
   if katz_deli != [ ]
-    puts "Currently serving #{katz_deli[0]} 
-    
+    puts "Currently serving #{katz_deli[0]}." 
     # remove first member of array, Omar.
+
   
   else
     puts "There is nobody waiting to be served!"
