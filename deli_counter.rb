c70610f2620f52b82f8d004ddb7f0c872b55e00b
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-06-04 03:39:08 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,3 +2,25 @@
 
 katz_deli = [ ]
 
+
+def line
+  #use .each for this, Omar.
+  if
+    puts "#{katz_deli[ ]} is currently number #{katz_deli[ ] + 1}" in line.
+  else
+    puts "The line is currently empty."
+end
+
+
+def take_a_number(katz_deli, name)
+  # add name to end of array, Omar.  And need to find a way to match index number to name for the puts command.
+  puts "Welcome, #{name}. You are number #{katz_deli[ ] + 1} in line."
+end
+
+def now_serving
+  if katz_deli != [ ]
+    puts "Currently serving #{katz_deli[0]}
+  else
+    puts "There is nobody waiting to be served!"
+end
+    
