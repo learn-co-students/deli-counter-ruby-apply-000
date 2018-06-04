3953857e6ed37341aa5d7aff743562210e32f5ae
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-06-04 03:42:38 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,13 +13,14 @@ end
 
 
 def take_a_number(katz_deli, name)
-  # add name to end of array, Omar.  And need to find a way to match index number to name for the puts command.
+  # add name to end of array, Omar.
   puts "Welcome, #{name}. You are number #{katz_deli[-1]} in line."
 end
 
 def now_serving
   if katz_deli != [ ]
-    puts "Currently serving #{katz_deli[0]}
+    puts "Currently serving #{katz_deli[0]} 
+    # remove first member of array
   else
     puts "There is nobody waiting to be served!"
 end
