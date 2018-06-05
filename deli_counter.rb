8158b301e945c340bf7272734df08f627b177446
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-06-05 06:57:38 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,7 @@ def line(katz_deli)
   if katz_deli == [ ]
    puts "The line is currently empty."
   else
-    
-    katz_deli.each_with_index do |name, index| puts "#{index + 1}. {#{name} " end
+    puts "The line is currently: "
   end
 end
 
