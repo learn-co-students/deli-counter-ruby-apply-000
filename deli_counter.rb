5c4bf05a66cb1e9ec5f00bd5ca4faac655410e89
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-06-05 03:37:10 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,29 +1,25 @@
 # Write your code here.
 
-katz_deli = [ ]
 
 
-def line
-  #use .each for this, Omar.  And need to somehow match each name with its index number + 1, for puts; maybe return a new_array listing, for example: "1. Ada.  2. Grace..."
-  if
-    puts "The line is currently: #{new_array}." # Note that the example sentence they provide does not end with  a sentence.
-  else
+def line(array)
+  katz_deli = [ ]
+  if katz_deli == [ ]
     puts "The line is currently empty."
+  else
+    array.each 
+    puts "The line is not empty."
+  end
 end
 
 
-def take_a_number(katz_deli, name)
-  # add name to end of array, Omar.
-  puts "Welcome, #{name}. You are number #{katz_deli[-1]} in line."
+
+def take_a_number(katz_deli, new_name)
+  katz_deli << new_name
+  puts "Welcome, #{new_name}. You are number #{katz_deli.size} in line."
 end
+
 
 def now_serving
-  if katz_deli != [ ]
-    puts "Currently serving #{katz_deli[0]}." 
-    # remove first member of array, Omar.
 
-  
-  else
-    puts "There is nobody waiting to be served!"
-end
-    
+
