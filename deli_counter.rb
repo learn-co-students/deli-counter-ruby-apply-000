280a6e1037721f640a3515e91724ae4b267e48d9
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-06-05 03:50:47 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,9 +20,14 @@ def take_a_number(katz_deli, new_name)
 end
 
 
-def now_serving
-  puts "Currently serving #{katz_deli[0]}."
+def now_serving(katz_deli)
+  if katz_deli != [ ]
+   puts "Currently serving #{katz_deli[0]}."
+  katz_deli.pop 
   
+  else
+    puts "There is nobody waiting to be served!"
+ end
 end
 
 
