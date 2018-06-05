1806f2e60dc4bdf13812154494faa5d83087d678
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-06-05 03:57:51 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,12 +22,11 @@ end
 
 def now_serving(katz_deli)
   if katz_deli != [ ]
-   puts "Currently serving #{katz_deli[0]}."
-  katz_deli.pop 
-  
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.pop
   else
     puts "There is nobody waiting to be served!"
- end
+  end
 end
 
 
