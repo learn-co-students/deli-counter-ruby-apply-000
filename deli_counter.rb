fae43c96913035f9f4bfd1d269260933afd195d9
yylaber <40437859+yylaber@users.noreply.github.com>
2018-06-20 16:54:14 -0400

    Update deli_counter.rb

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 def line(line)
   if line.empty?
     puts "The line is currently empty."
@@ -29,18 +29,3 @@ def now_serving(line)
   end
   # line
 end
-=======
-# Write your code here.
-katz_deli = []
-def line(katz_deli)
-  if katz_deli.empty?
-    puts "The line is currently empty."
-  else
-    i = 0
-    while i < katz_deli.length
-      puts "#The line is currently:  {i+1}. #{katz_deli[i]}"
-      i += 1
-    end
-  end
-end
->>>>>>> a657453c255d4261b970a272b25e324152011198
