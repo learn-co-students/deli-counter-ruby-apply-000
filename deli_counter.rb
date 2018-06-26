533caaf6ed74ac3ea39f423ac2d9bd01730fccf3
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-06-26 04:28:31 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,16 @@
 # Write your code here.
-
-
-
 def line(katz_deli)
   if katz_deli == [ ]
     puts "The line is currently empty."
-  else
-    puts "The line is currently: " 
-  end
+  else 
+    string = "The line is currently:"
+    x = 1
+    while x <= katz_deli.size
+    string << " #{x}. #{katz_deli[x - 1]}"
+    x += 1 
+    end
+    puts "#{string}" 
+  end 
 end
   
 
