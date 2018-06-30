9075548c034aacef5317836a6bc35382a6406640
Sherzod Karimov <karimovj@gmail.com>
2018-06-30 07:02:48 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,7 @@
 katz_deli = []
 
+#line method
+
 def line(katz_deli)
  line_arr = []
   if katz_deli.length == 0
@@ -12,11 +14,15 @@ end
 puts "The line is currently: #{line_arr.join(" ")}"
 end
 
+#take a number method 
+
 def take_a_number(katz_deli, name)
   katz_deli << name
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
   end
   
+#now serving method
+
 def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
