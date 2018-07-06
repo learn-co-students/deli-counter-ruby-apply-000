589d198b92f8f16e41fc99511e99b27615bb456a
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-07-06 01:15:35 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,12 +12,32 @@ def line(katz_deli)
   end 
 end
   
+  
+
+# assign each new person a number
+# add them to the line as their number
+# tell them what their number is
+
+def take_a_number_2(katz_deli, new_name)
+  # Do I need to write "loop do" here?
+    num = 0
+    katz_deli << new_name
+    num += 1 
+# if I can count the number of times that a new_name gets shoveled into katz_deli, and set it = to "num", then it's a simple matter of
+    puts "You are number #{num}."
+  # Do I need to write "end" here?
+  
+end
+
+
 
 def take_a_number(katz_deli, new_name)
   katz_deli << new_name
   puts "Welcome, #{new_name}. You are number #{katz_deli.size} in line."
 end
 
+  
+  
 
 def now_serving(katz_deli)
   if katz_deli != [ ]
