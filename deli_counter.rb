0dc5c215cee7a64b3dd4f24562e276eb72b2d043
Peter Kreppein <pmkreppein@gmail.com>
2018-07-25 03:01:13 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+katz_deli = []
+def line(array)
+  count = array.count
+
+  case count
+  when count <= 0
+    puts "The line is currently empty."
+  when count > 0
+    puts "The line currently has #{count} people waiting."
+  else
+    puts "An error has occurred, please contact support. Message: Invalid arguement count passed."
+end
+
+def take_a_number(array, name)
+  array.push(name)
+  puts "#{name}, #{array.count()}"
+end
+
+def now_serving(array)
+  array
+end
