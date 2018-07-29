fc4425effe076250f9122205e779dcb1b47abf4e
Shuo Wang <gken90@gmail.com>
2018-07-29 22:19:55 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+
+katz_deli = []
+
+def line()
+  if katz_deli != [] 
+    puts "Welcome, katz_deli[0]. You are number katz_deli[index+1] in line"
+  else 
+    puts "The line is currently empty."
+  end 
+  end
+end 
+
+
+  # take_a_number(katz_deli, "Ada") #=> Welcome, Ada. You are number 1 in line.
+  # take_a_number(katz_deli, "Grace") #=> Welcome, Grace. You are number 2 in line.
+  # take_a_number(katz_deli, "Kent") #=> Welcome, Kent. You are number 3 in line.
