7bd90d6933613b117daacfe61659af1a95ac2b24
Nasser Sanou <nassersanou23@gmail.com>
2018-07-30 07:48:57 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+# theLine = []
+Katz_Deli =[]
+
+
+def line(customers)
+  lineArray = []
+  if customers.length == 0
+    puts "The line is currently empty."
+  else
+    cunstomers.each.with_index do |name, index|
+      lineArray.push("#{name} #{index}")
+    end
+    puts "The next in line is: #{lineArray.join(" ")}"
+  end
+end
+
+
+# def take_a_number()
+# end
+
+# def now_serving()
+# end 
