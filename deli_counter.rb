71292ff8cd59c7040583fe9ba3192e4c5a0c29fe
oneilld136 <oneilld136@gmail.com>
2018-08-02 04:47:51 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,19 +7,21 @@ def line(x)
   else
     x.each.with_index(1) do |name, index|
       line_array.push("#{index}. #{name}")
+    end
+    puts "The line is currently: #{line_array.join(' ')}"
+  end
 end
-    puts "The line is currently: #{line_array.join(', ')}"  // 'Name,Index'
-end
+
 def take_a_number (katz_deli, name)
-  katz_deli['name'].push(name)
-  p = katz_deli.index(name)+1
-  puts "Welcome,#{name}. You are number #{katz_deli.length}".
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+
 def now_serving(array)
   if array.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{array[1]}."
+    puts "Currently serving #{array[0]}."
     array.shift
   end
 end
