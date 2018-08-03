4a415460c2058106adeac6d8b60acd2e9acd67fc
alexanderchao123 <chao.alexander.123@gmail.com>
2018-08-03 23:06:27 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,27 +1,27 @@
 katz_deli = []
 
-def line(katz_deli)
-  if katz_deli.empty?
+def line(queue)
+  if queue.empty?
     puts "The line is currently empty."
   else
-    queue = "The line is currently:"
-    katz_deli.each_with_index do |name, index|
-      queue.concat(" #{index+1}. #{name}")
+    queue_message = "The line is currently:"
+    queue.each_with_index do |name, index|
+      queue_message.concat(" #{index+1}. #{name}")
     end
-    puts queue
+    puts queue_message
   end
 end
 
-def take_a_number(array, name)
-  array.push(name)
-  puts "Welcome, #{name}. You are number #{array.count} in line."
+def take_a_number(queue, name)
+  queue.push(name)
+  puts "Welcome, #{name}. You are number #{queue.count} in line."
 end
 
-def now_serving(array)
-  if array.empty?
+def now_serving(queue)
+  if queue.empty?
     puts "There is nobody waiting to be served!"
   else
-    servee = array.shift
+    servee = queue.shift
     puts "Currently serving #{servee}."
   end
 end
