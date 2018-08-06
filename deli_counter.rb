2d31972e4ead65ed3361f7cbc665f5bfc7cc37c3
Isabelle Pino <ipino0128@gmail.com>
2018-08-06 19:10:23 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,24 +1,26 @@
-# Write your code here.
 #line
+#method to show everyone their current place in line
 def line(katz_deli)
     if katz_deli.empty? == true
       puts "The line is currently empty."
     else
-      line_array = "The line is currently:"
+      current_line = "The line is currently:"
       katz_deli.each.with_index(1) do |person, index|
-        line_array << " #{index}. #{person}"
+      current_line << " #{index}. #{person}"
       end
-      puts line_array
+      puts current_lines
     end
 end
 
 #take_a_number
+#method to take in new customer and calls out that person's name and their position in line
 def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
 end
 
 #now_serving
+#method to call out next person in line and then remove them from the front 
 def now_serving(katz_deli)
   if katz_deli.empty? == true
     puts "There is nobody waiting to be served!"
