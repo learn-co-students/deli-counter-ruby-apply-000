9e20a0876f946a8d6e44f30b63e0dde55a0f6297
Min Hee  Park <minheekangg@gmail.com>
2018-08-26 23:23:44 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+
+
+def line(arr)
+
+
+  if arr.length == 0
+      puts "The line is currently empty."
+  else
+    i=0
+    result = ""
+    while i < arr.length
+      result = result + " " + i + ". " + arr[i]
+    i+=1
+  end
+  end
+  if result[0] == " "
+    result[0] = ""
+  end
+    return "The line is currently: " + result
+
+end
