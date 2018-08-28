7007e86ebc42f1a27ec31d5dc688ffb329784f61
Julianna Tetreault <juliannatetreault@icloud.com>
2018-08-28 04:33:10 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ end
 
 def take_a_number(katz_deli, name)
  katz_deli.push(name)
- puts "Welcome #{name}. You are #{katz_deli.length} in line." 
+ puts "Welcome, #{name}. You are number #{katz_deli.length} in line." 
 end
 
 def now_serving(katz_deli, name)
