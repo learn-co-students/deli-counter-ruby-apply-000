aac4e07a247a0206beea5004d6e4c859ea5446fe
sun5hine <af5kh@yahoo.com>
2018-09-23 21:26:42 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def line(arr)
+ if arr.empty?
+puts "The line is currently empty."
+else
+newArr = []
+arr.each_index {|x| newArr.push("#{x+1}. #{arr[x]}")} 
+puts "The line is currently: #{newArr.join(" ")}"
+ end
+end
+
+def take_a_number(katz_deli, name)
+katz_deli.push(name)
+puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+learn 
+ 
