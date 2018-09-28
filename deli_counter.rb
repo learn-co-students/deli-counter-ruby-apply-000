2e009ab06bbaf73e6fb3d8d9a4aaa88095c201d9
Andrew Wang <andrewwang39@gmail.com>
2018-09-28 20:47:57 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+
+def line(arr)
+  if arr.length == 0
+    puts "The line is currently empty."
+  else
+    str = "The line is currently: "
+    arr.each_with_index{|name, idx|
+      str += "#{idx+1}. #{name} "
+    }
+  end
+  str
+end
+
+def take_a_number(arr, name)
+
+end
+
+def now_serving(arr)
+
+end
