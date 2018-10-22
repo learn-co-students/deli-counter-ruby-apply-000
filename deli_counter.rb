e616bfe62310907438205a2451f3364c9743b0f9
Simon Balean <simonbalean@gmail.com>
2018-10-22 18:16:19 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,20 +1,18 @@
 # Write your code here.
 
-katz_deli = []
 
-def line(katz_deli)
-  if katz_deli == []
+def line(katz_deli = [])
+  if katz_deli.empty?
     puts "The line is currently empty."
   else
-    line = []
-    line_number = 1
-    katz_deli.each do |line_name|
-      line << "#{line_number}. #{line_name}"
-      line_number = line_number + 1
+    line = ""
+    katz_deli.each.with_index(1) do |line_name, index|
+      line << " #{index}. #{line_name}"
     end
-      puts "The line is currently: #{line.join(" ")}"
+      puts "The line is currently:#{line}"
   end
 end
+
 
 
 def take_a_number(katz_deli, name)
@@ -23,11 +21,31 @@ def take_a_number(katz_deli, name)
 end
 
 
+
 def now_serving(katz_deli)
-  if katz_deli == []
+  if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli[0]}."
     katz_deli.shift
   end
 end
+
+
+
+# MANUAL METHOD w/o <each.with_index>
+# def line(katz_deli = [])
+#   if katz_deli == []
+#     puts "The line is currently empty."
+#   else
+#     line = []
+#     line_number = 1
+#     katz_deli.each do |line_name|
+#       line << "#{line_number}. #{line_name}"
+#       line_number = line_number + 1
+#     end
+#       puts "The line is currently: #{line.join(" ")}"
+#   end
+# end
+
+
