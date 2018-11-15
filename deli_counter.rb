90b6c21f15ea52ece7eef9c3fc1b60396e0518d2
Shaundra <s.m.crittenden@gmail.com>
2018-11-15 03:11:44 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,15 +2,14 @@ def line(array)
   if array.empty?
     puts "The line is currently empty."
   else
-    line_state = "The line is currently: "
-    array.map.with_index { |name, idx| line_state << "#{idx + 1}. #{name} "}
-    puts line_state.strip
+    line_state = array.map.with_index { |name, idx| "#{idx + 1}. #{name}"}.join(" ")
+    puts "The line is currently: #{line_state}"
   end
 end
 
 def take_a_number(array, name)
   array.push(name)
-  puts "Welcome, #{name}. You are number #{array.index(name) + 1} in line."
+  puts "Welcome, #{name}. You are number #{array.size} in line."
 end
 
 def now_serving(array)
