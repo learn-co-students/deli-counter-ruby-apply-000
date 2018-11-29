86ee03174baff77d7f218fd6d305714e7dc383c9
bionic-request-3438 <thunderhorsesemail@gmail.com>
2018-11-29 06:15:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def line(katz_line) 
+  if katz_line.length == 0 
+    puts "The line is currently empty." 
+  else 
+    prompt = "The line is currently:" 
+
+  katz_line.each_with_index do |ele, idx| 
+    prompt += " #{idx.to_i + 1}. #{ele}"
+  end 
+    puts "#{prompt}"
+  end 
+end 
+
+
+def take_a_number(line, person) 
+  line.push(person) 
+  puts "Welcome, #{person}. You are number #{line.length} in line."
+end 
+
+
+def now_serving(line) 
+  if line.length == 0 
+    puts "There is nobody waiting to be served!" 
+  else 
+    puts "Currently serving #{line[0]}."
+    line.shift()
+  end 
+end 
