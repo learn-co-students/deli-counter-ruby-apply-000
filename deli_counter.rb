c167ff6d8dc680c91c3a349c735890d1fb95e6a4
shivani brijmohan <sbrij001@yahoo.com>
2018-12-02 00:47:20 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,10 +7,10 @@
 # if the line array is empty #line should say the line is empty
 # if there are people in the line #line should display the current line
 # _________
-# create a new array that I can use to manipulate
+# create a new array
 # use a conditional to check the length of the katz deli
-# use the each_with_index to iterate thru the array
-# if length > 0, current line format should diplay current line: + the name and it's (index + 1)
+# If the length is > 0 use the each_with_index to iterate thru the array
+# puts current line:(index + 1) + the name
 # else display "The line is currently empty."
 # Make sure to convert the array into a string before returning using join
 
@@ -31,12 +31,13 @@ end
 # #take_a_number should call out (puts) the person's name along with their position in line.
 # Top-Tip: Remember that people like to count from 1, not from 0 ("zero") like computers.
 # ________
-# if #take_a_number is empty it should add a person to the line
-# if there are already people in the line #take_a_number should add a person to the end of the line.
+# #take_a_number should add a person to the end of the line.
 # #take_a_number needs to be able to add mult people in a row.
 # hint for me to use the .push method
-# use the .index() method to locate the index where the ele is found and add 1
-# puts the message using string interpolation.
+# use the .index() method to locate the index where the ele is found + 1
+# store the index in a var
+# puts Welcome, person . You are number position in line.
+
 
 def take_a_number(katz_deli, person)
     katz_deli.push(person) # will evaulate to the katz_deli arr
@@ -50,7 +51,9 @@ end
 # _________
 # if there are people in the line
 # #now_serving should serve the first person in line and remove them from the queue
-# #hint to use shift method
+# hint to use shift method
+# store the ele in a var
+# puts cureently serving + the person its serving
 # If there is nobody in line puts "There is nobody waiting to be served!".
 
 def now_serving(katz_deli)
