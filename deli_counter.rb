c878b889900bf4fbf31da0f9ed6af1fecc3ac929
funny-reference-4499 <davidbernier91@gmail.com>
2019-01-21 18:18:53 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,18 +1,19 @@
-def line(array)
-  return puts "The line is currently empty." if array.empty?
+def line(arr1)
+  return puts "The line is currently empty." if arr1.empty?
 
   customer_line = "The line is currently:"
-    array.each.with_index(1) {|name, i| customer_line.concat(" #{i}. #{name}")}
-    puts  customer_line
+  arr1.each.with_index(1) {|name, i| customer_line.concat(" #{i}. #{name}")}
+  puts  customer_line
 end
 
-def take_a_number(array, name)
-    array.push(name)
-    puts "Welcome, #{name}. You are number #{array.size} in line."
+def take_a_number(arr1, name)
+  arr1.push(name)
+  puts "Welcome, #{name}. You are number #{arr1.size} in line."
 end
 
-def now_serving(array)
-return puts "There is nobody waiting to be served!" if array.empty?
-    puts "Currently serving #{array.first}."
-    array.delete_at(0)
+def now_serving(arr1)
+  return puts "There is nobody waiting to be served!" if arr1.empty?
+
+  puts "Currently serving #{arr1.first}."
+  arr1.delete_at(0)
 end
