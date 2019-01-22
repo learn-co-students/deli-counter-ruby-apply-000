f1908380c6e8ed7173b728273df22bd69ad67220
Sarah Daniels <ssdaniels13@gmail.com>
2019-01-22 10:38:36 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,15 +11,10 @@ def line(x)
     puts "The line is currently: #{line_array.join(" ")}"
   end
 end
-
-
-
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
-
-
 def now_serving(array)
   if array.empty?
     puts "There is nobody waiting to be served!"
