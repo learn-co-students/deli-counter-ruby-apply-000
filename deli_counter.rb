3afc505ee82df01330dd552cc412576b7d559634
Benjamin Reilly <benjamin.reilly@gmail.com>
2019-01-25 00:56:51 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,7 @@
-katz_deli = []
 
 def line(katz_deli)
   if  katz_deli.empty? 
-    output= "The line is currently empty."
+    output = "The line is currently empty."
   else
     output = "The line is currently:"
     katz_deli.each_with_index do |name, i| 
