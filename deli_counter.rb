cd4219c1e4453d35bc3022bf17babfc97e1b45f9
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-07 15:38:09 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+katz_deli = []
+def line(arr)
+  if(arr.length == 0)
+    return "The line is currently empty."
+  else
+    return arr.length
+  end
+end
+
+def take_a_number(arr, name)
+  puts 
+end
