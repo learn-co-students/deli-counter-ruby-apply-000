ef0ef283b7eb46751f438135955332a93a66dd3d
clever-genre-5225 <jed.spears7@gmail.com>
2019-02-08 00:55:15 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+katz_deli = []
+
+def line(deli)
+  if deli.empty?
+    line_str = "The line is currently empty."
+  else
+    line_str = "The line is currently:"
+    deli.each_with_index { |person, index| line_str += " #{index + 1}. #{person}" }
+  end
+  puts line_str
+end 
+
+def take_a_number(deli, name)
+  deli.push(name)
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
+end 
+
+def now_serving(deli)
+  if deli.empty?
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{deli.shift}."
+  end
+end 
+
+
+    
