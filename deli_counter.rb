e324f593f0e38500e45c2a015db23aaef02bb9c7
Ryan Rowland <rbillrowland@gmail.com>
2019-02-22 00:56:30 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,21 +5,22 @@ def line(line)
     puts "The line is currently empty."
   else
     count = 1
-    puts "The line is currently: "
+    output = ""
     line.each do |person|
-      puts `#{count}. #{person}`
-      count++
+      output = "#{output} #{count}. #{person}"
+      count += 1
     end
+    puts "The line is currently:#{output}"
   end
 end
 
 def take_a_number(line, person)
   count = 1
   line.each do
-    count++
+    count += 1
   end
   line << person
-  puts `Welcome, #{person}. You are number #{count} in line.`
+  puts "Welcome, #{person}. You are number #{count} in line."
 end
 
 def now_serving(line)
@@ -27,6 +28,6 @@ def now_serving(line)
     puts "There is nobody waiting to be served!"
   else
     person = line.shift
-    puts `Currently serving #{person}.`
+    puts "Currently serving #{person}."
   end
 end
