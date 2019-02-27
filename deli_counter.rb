cbd0eb70897e92f855ed821285a4780c274ee43c
darling-trace-1821 <kmanrique93@gmail.com>
2019-02-27 19:56:02 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+katz_deli = []
+
+def line(shop_name)
+  if shop_name == []
+    puts "The line is currently empty."
+  else
+    fragment = ""
+    shop_name.each_with_index do |customer, idx|
+    fragment << " #{idx + 1}. #{customer}"
+  end
+  puts "The line is currently:#{fragment}"
+  end
+end 
+
+def take_a_number(shop_name, name)
+  shop_name.push(name)
+  place_in_line = shop_name.length
+  puts "Welcome, #{name}. You are number #{place_in_line} in line."
+end
+  
+def now_serving(shop_name)
+  if shop_name == []
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving " + shop_name[0] + "."
+    shop_name.delete(shop_name[0])
+  end
+end
