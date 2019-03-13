4c2444d56132d05e443d6ac088fe81778693c1d5
Sohaib Mubashir <sohaibmh@yahoo.com>
2019-03-13 22:20:03 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+katz_deli = []
+def deli_line(array)
+  if array.empty?
+    print "The line is empty."
+  elsif
+    puts "The line is currently: "
+  elsif
+    array.map.with_index{|x, i| print x + " ", i+1, + ". "}
+  end
+end
+print deli_line(katz_deli)
