e838777ca0ec1ba46d64cf97555f3ae65d23c1dc
lite-authenticator-7506 <kurianvijay7@gmail.com>
2019-03-15 14:04:50 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+katz_deli = []
+
+
+def take_a_number(array, name)
+array << name  
+array.length
+ puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+# add new incoming element
+# return index number of the element 
+
+
+
+#def line(array, element=0)
+#if array.length > 0 
+#puts array.index(element)
+#else 
+#  puts "The line is currently empty"
+#end
+#end
+
+
+#def now_serving(i)
+#next= i.length + 1
+#end
+  
+  
+   
+  
