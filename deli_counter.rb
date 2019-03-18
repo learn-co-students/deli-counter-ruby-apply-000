1449aef02a3934496199fd903e6a6c131e74b38e
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-18 13:38:06 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+katz_deli = ["1", "2", "3", "4", "5", "6", "7"].
+
+def position 
+  katz_deli[place]
+end
+
+line_size = katz_deli.length
+
+def line(name, position) 
+  if line_size > katz_deli.length 
+    katz_deli.each_with_index do |katz_deli|
+      puts katz_deli, position 
+    katz_deli += 1  
+  elsif line_size = 0
+    puts "The line is currently empty."
+  end
+end 
