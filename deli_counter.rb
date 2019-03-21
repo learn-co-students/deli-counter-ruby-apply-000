f84d44aa1659ea3f0a9da108264acd7090fad418
Ay Has <ahashim983@yahoo.com>
2019-03-21 20:56:42 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,32 @@
 katz_deli = []
 
+def line(katz_deli)
+  if katz_deli == []
+    puts "The line is currently empty."
+  else
+    line_format = String.new
+    katz_deli.each do |person|
+        index = (katz_deli.index(person).to_i)+1
+        line_format.concat(" #{index}. #{person}")
+   end
+   puts "The line is currently:#{line_format}"
+  end
+end
+
+
 def take_a_number(katz_deli, new_person)
   katz_deli.push(new_person)
-  puts "Welcome #{new_person}. You are number #{katz_deli[new_person]} in line."
+  index = (katz_deli.index(new_person).to_i)+1
+  puts "Welcome, #{new_person}. You are number #{index} in line."
 end
 
 def now_serving(katz_deli)
   if katz_deli == []
     puts "There is nobody waiting to be served!"
   else
-    puts "Now serving #{katz_deli[0]}"
+    puts "Currently serving #{katz_deli[0]}."
     katz_deli.shift()
+  end
 end
 
 
