11249af9acdd8f462aa1ca47ba34483ed45084f4
Ay Has <ahashim983@yahoo.com>
2019-03-21 20:08:56 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
+katz_deli = []
+
+def take_a_number(katz_deli, new_person)
+  katz_deli.push(new_person)
+  puts "Welcome #{new_person}. You are number #{katz_deli[new_person]} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli == []
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Now serving #{katz_deli[0]}"
+    katz_deli.shift()
+end
+
+
+
+
+
 # Write your code here.
