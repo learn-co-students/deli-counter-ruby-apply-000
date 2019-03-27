ee8f9adbac9e8b322727872a1fbbaefe233d3a70
Francis Mendez <fmendez884@gmail.com>
2019-03-27 14:53:44 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,18 +2,24 @@
 
 def line(katz_deli)
   deli = []
+
     i = 0
+
   while (i < katz_deli.length)
-    deli << "#{i+1}. #{deli_line[i]}"
+    deli << "#{i+1}. #{katz_deli[i]}"
     i +=1 
   end
   
   if katz_deli.length == 0 
-    puts "The line is currently empty"
+    puts "The line is currently empty."
   else
-    return "The line is currently: #{deli.join}" 
+    return "The line is currently: #{deli.join(', ')}." 
   end
 end
+
+
+
+
 
 # def line(katz_deli)
 # line = []
@@ -30,4 +36,7 @@ end
 #     end
 # end 
 
+
+#     puts "The line is currently: #{deli.join}"  
+# end
 
