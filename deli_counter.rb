c3d588aae2a73e298bea2eb21fbaae5ab7e02b72
Sohaib Mubashir <sohaibmh@yahoo.com>
2019-03-27 20:44:11 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,26 +1,37 @@
 
 katz_deli = ["Logan", "Avi", "Spencer"]
 
-
-def deli_line(array)
+def line(array)
   if array.empty?
     puts "The line is currently empty."
-  elsif
-    puts "The line is currently: "
-  elsif
-    array.map.with_index{|x, i| print x + " ", i+1, + ". "}
+  else
+    result = ""
+    array.each.with_index{|element, index| result += "#{index+1}. #{element} "}
+  puts "The line is currently: " + result.strip
   end
 end
 
-puts deli_line(katz_deli)
+line(katz_deli)
+puts
 
 def now_serving(array)
   if array.empty?
     puts "There is nobody waiting to be served!"
   elsif
-    puts "Now serving"
-  elsif
-  puts array[1]
+    puts "Currently serving #{array[0]}."
+  end
+  array.shift
 end
+
+now_serving(katz_deli)
+puts
+
+def take_a_number(array, element)
+array.push(element)
+puts "Welcome, #{element}. You are number #{array.length} in line."
 end
-puts now_serving(katz_deli)
+
+take_a_number(katz_deli, "Hayat")
+puts
+
+line(katz_deli)
