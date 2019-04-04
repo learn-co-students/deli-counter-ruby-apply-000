8da873433923997978f8dec5a86f53baa99c2c2c
Maija Barrins <maija.barrins@gmail.com>
2019-04-04 08:50:45 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,19 +2,19 @@ def line(array)
   if array.empty?
     puts "The line is currently empty."
   else
-    puts "The line is currently: " + array.each_with_index.map{|i,j| (j+1).to_s + ". " + i.to_s}.join(" ")
+    puts "The line is currently: " + array.map{|i| "#{array.index(i)+1}. #{i}"}.join(" ")
   end
 end
 
 def take_a_number(array, string)
   array << string
-  puts "Welcome, %s. You are number %s in line." % [string, array.length]
+  puts "Welcome, #{string}. You are number #{array.length} in line."
 end
 
 def now_serving(array)
   if array.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving %s." % [array.shift]
+    puts "Currently serving #{array.shift}."
   end
 end
