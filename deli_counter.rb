3518335681eedc28d475fd50b53a97bdfcf4957f
Awoh Negheie <awohnegheie@gmail.com>
2019-04-25 18:36:01 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def line(katz_deli)
+  katz_deli = []
+  puts "The line is currently empty."
+end
+
+def
+puts take_a_number
+katz_deli = [
+  1 = "Ada"
+  2 = "Grace"
+  3 = "Kent"];
+
+end
+
+def
+puts now_serving
+
+end
