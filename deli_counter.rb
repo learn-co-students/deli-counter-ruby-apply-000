30bba9d6f6a867a5a9cf708830e58338c30396fb
Jackson Singleton <jsingleton12321@yahoo.com>
2019-04-29 12:39:02 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+
+def line(loc)
+  if loc.length == 0
+    puts "The line is currently empty."
+  else
+    string = ""
+    string += "The line is currently: "
+    for i in 0..loc.length-2 do
+      string += "#{i+1}. #{loc[i]} "
+    end
+    string+= "#{loc.length}. #{loc[loc.length-1]}"
+    puts string
+  end
+end
+
+def take_a_number(line, person)
+  line.push(person)
+  puts "Welcome, #{person}. You are number #{line.length} in line."
+end
+
+def now_serving(line)
+  if line.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{line[0]}."
+    line.shift
+  end
+end
