baad68235ada4f0c4ee8f50cdeda33a97959216c
Iris S <irisiseve@gmail.com>
2019-05-16 14:57:19 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,27 +1,57 @@
 # Write your code here.
 
-def line(people_in_line)
-  line_string = "The line is currently:"
-  if people_in_line.length > 0
-    people_in_line.each_with_index do |person, index|
-        line_string += " #{index + 1}. #{person}"
-    end
-  else
-    line_string = "The line is currently empty."
+def line(deli_line)
+  string = "The line is currently:"
+
+  if deli_line.empty?
+    string = "The line is currently empty."
   end
-  puts line_string
+
+  deli_line.each_with_index do |person, index|
+    string += " #{index + 1}. #{person}"
+  end
+
+  puts string
 end
 
-def take_a_number(line, name)
-    line << name
-    puts "Welcome, #{name}. You are number #{(line.index(name)) + 1 } in line."
+def take_a_number(deli_line, name)
+  deli_line << name
+  puts "Welcome, #{name}. You are number #{deli_line.length} in line."
 end
 
-def now_serving(people_in_line)
-    if people_in_line.length > 0
-        puts "Currently serving #{people_in_line[0]}."
-        people_in_line.shift()
-    else
-        puts "There is nobody waiting to be served!"
-    end
+
+def now_serving(deli_line)
+  if deli_line.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli_line.first}."
+    deli_line.shift()
+  end
 end
+
+
+# def line(people_in_line)
+#   line_string = "The line is currently:"
+#   if people_in_line.length > 0
+#     people_in_line.each_with_index do |person, index|
+#         line_string += " #{index + 1}. #{person}"
+#     end
+#   else
+#     line_string = "The line is currently empty."
+#   end
+#   puts line_string
+# end
+#
+# def take_a_number(line, name)
+#     line << name
+#     puts "Welcome, #{name}. You are number #{(line.index(name)) + 1 } in line."
+# end
+#
+# def now_serving(people_in_line)
+#     if people_in_line.length > 0
+#         puts "Currently serving #{people_in_line[0]}."
+#         people_in_line.shift()
+#     else
+#         puts "There is nobody waiting to be served!"
+#     end
+# end
