cdd5c845ac7105223040c44e31950efd03cac89f
Tegan Sims <tegansims@hotmail.com>
2019-05-23 13:43:59 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,24 +2,34 @@
 katz_deli = []
 
 def line(place = "The line is currently empty")
-  puts "The line is currently: #{katz_deli}."
-end
+  print "The line is currently: "
+    n=0 
+    while n < place.length
+      print "#{n+1}. #{place[n]} "
+      n = n + 1
+    end
+  end
 
 
-def take_a_number(katz_deli, name)
-  puts "Welcome, #{name}, you are number #{katz_deli.length+1} in line"
-  katz_deli.push(name)
+def take_a_number(list, name)
+  puts "Welcome, #{name}, you are number #{list.length+1} in line"
+  list.push(name)
+  return
 end
 
 def now_serving(next_person = "There is nobody waiting to be served!")
-  puts "Currently serving #{katz_deli[0]}."
-  katz_deli.pop
+  puts "Currently serving #{next_person[0]}."
+  next_person.shift
+  return
 end
 
 take_a_number(katz_deli, "Ada")
 take_a_number(katz_deli, "Grace")
 take_a_number(katz_deli, "Kent")
-puts katz_deli
-# puts line(katz_deli)
+puts line(katz_deli)
 puts now_serving(katz_deli)
+puts line(katz_deli)
 puts take_a_number(katz_deli, "Matz")
+puts line(katz_deli)
+puts now_serving(katz_deli)
+puts line(katz_deli)
---
+++ test.rb
@@ -0,0 +1,17 @@
+katz_deli = ["bob", "dude", "lady"]
+
+
+def line(name = "The line is currently empty.")
+  n=0 
+  x = name.length
+  print "The line is currently: "
+    while n < x
+      print "#{n}. #{name[n]} "
+      n = n + 1
+    end
+end
+  
+
+  
+puts line(katz_deli)
+
