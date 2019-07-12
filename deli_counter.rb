9a60367a7a0c404bd06686f77052f75c33d68045
Mason Balbera <mhbalbera@gmail.com>
2019-07-12 18:31:28 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def line(katz_deli)
+  order = "The line is currently:"
+  array = []
+  if katz_deli.empty?
+    puts "The line is currently empty." 
+  else
+    katz_deli.each_with_index do |ele, i|
+      n = i + 1 
+      order +=" #{n}. #{ele}"
+    end
+    puts order
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!" 
+  else
+    puts "Currently serving #{katz_deli.shift}."
+  end
+end
