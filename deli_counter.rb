297ff466e7254910bc38b631292f3b3719ed8b1d
Israel Sanchez <israel.pro1994@gmail.com>
2019-07-19 05:42:07 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,43 @@
 # Write your code here.
+
+#  ["Logan", "Avi", "Spencer"]
+
+def line(names)
+  if names == [] 
+    puts "The line is currently empty."
+  else 
+    position = 0
+    message = "The line is currently:"
+    
+    names.each do | name |
+      position = position + 1 
+      message = message + " #{position}. #{name}"
+    end
+    puts message
+  end
+end
+
+
+
+
+def take_a_number(names,name)
+    names.push(name)
+    position = 0
+    names.each do |name| 
+      position = position + 1
+    end
+    puts "Welcome, #{name}. You are number #{position} in line."
+end
+
+
+
+def now_serving(names)
+  
+  if names == []
+    puts "There is nobody waiting to be served!"
+  else 
+      message = "Currently serving "
+      puts message + "#{names[0]}."
+      names.shift
+  end 
+end
