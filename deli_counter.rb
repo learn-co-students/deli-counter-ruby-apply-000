55f039ab131cff4e4f1cacb5e64ebdc33829a132
Robert Eggen <rjegrad@yahoo.com>
2019-07-19 17:57:36 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,3 +24,23 @@ def # add_element_to_starrt_array
   puts[jerry Seinfeld
   else if ["There is nobody waiting to be served!""]
   end
+katz_deli =[]
+takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
+takeANumber(katzDeliLine, "George"); // "Welcome, George, You are number 2 in line"
+takeANumber(katzDeliLine, "Ginger"); //"welcome, Ginger, You are numner 3 in line"
+takeANumber(katzDeliLine, "Bob"); // "Welcome, Bob, You are number 4 in line"
+takeANumber(katzDeliLine, "Charles"); // "Welcome Charles, You are number 5 in line"
+takeANumber(katzDeliLine, "Mary"); // "Welcome Mary, You are number 6 in line"
+currentLine(katzDeliLine), //"The Line is currently: 1, Ada, 2. George, 3. Ginger, 4. Bob, 5.Charles, 6. Mary"
+nowServing(katzDeliLine); //"currently serving Ada"
+currentLine(katzDeliLine), // "The line is currently serving" 2. George 3. Ginger, 4. Bob, 5 Charles, 6. Mary
+nowServing(katzDeliLine); // "currently serving George"
+currentLine(katzDeliLine); 3. Ginger, 4. Bob, 5. Charles, 6. Mary
+nowServing(katzDeliLine); // "currently serving Ginger"
+currentLine(katzDeliLine); 4. Bob, 5. Charles, 6. Mary
+nowServing(katzDeliLine); // "currently serving Bob"
+currentLine(katzDeliLine); 5. Charles, 6. Mary
+nowServing(katzDeliLine); // "currently serving Charles"
+currentLine(katzDeliLine); 6. Mary
+nowServing(katzDeliLine); "currently serving Mary"
+nowServing(katzDeliLine); "line is currently empty"
