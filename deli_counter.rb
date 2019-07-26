b0c5edb940fc621b9fe7217306b0fe58dbf70aad
Chen Wang <wangcees@gmail.com>
2019-07-26 17:49:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,4 +12,9 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  else
+    katz_deli.each { || }
+  end
 end
