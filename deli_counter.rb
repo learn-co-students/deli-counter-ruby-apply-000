d2d2d83f8b19c2230b6893ef146140bdbf875772
Chen Wang <wangcees@gmail.com>
2019-07-26 18:28:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,5 +2,5 @@
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts  "Welcome, #{name}. You are number" + katz_deli.length + " in line."
+  puts  "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
