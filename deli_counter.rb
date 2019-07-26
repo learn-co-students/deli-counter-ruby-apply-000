fe07a4264fe4d6372cd8b6f002ff9ff7b7775778
Chen Wang <wangcees@gmail.com>
2019-07-26 18:59:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     print "The line is currently: "
-    katz_deli.each { |name, index| 
+    katz_deli.each { |name, index + 1| 
       print "#{index}. #{name}" }
   end
 end
