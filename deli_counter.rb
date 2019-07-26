5e9ecd821f01c9dd7b7d0199651595c774cb313a
Chen Wang <wangcees@gmail.com>
2019-07-26 18:32:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,3 +4,12 @@ def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts  "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+
+def now_serving (katz_deli)
+  if katz_deli.length == 0 
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift()
+  end
+end
