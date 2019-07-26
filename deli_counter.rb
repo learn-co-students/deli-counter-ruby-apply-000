a69b7f3ef390e4114553b86cddc2ce66625ee8f2
Chen Wang <wangcees@gmail.com>
2019-07-26 17:49:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,11 @@
 # Write your code here.
 
 def line (katz_deli)
-  if katz_deli
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  else
+    katz_deli.each { || }
+  end
 end
 
 def take_a_number(katz_deli, name)
