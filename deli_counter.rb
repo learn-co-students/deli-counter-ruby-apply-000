1de50f5c9aec3b454e35ec4eb07bdcb897c758ca
calm-block-7100 <hish.b92@gmail.com>
2019-08-03 11:06:15 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+katz_deli = [] #currently no one in the line
+
+def take_a_number(katz_deli,"john")
+  puts "Welcome John, you are number 0 in line."
+end
+
+def now_serving(katz_deli, "Andy")
+  puts "Welcome Andy, you are number 1 in line."
+end
