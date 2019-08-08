6dca29057a8b0a126c4bf2066eb89df3c875cb81
bionic-chipset-2986 <janaeelisa@gmail.com>
2019-08-08 09:43:18 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+
+def line(deli)
+  if deli.length == 0
+    puts "The line is currently empty."
+  else
+    new_deli = []
+    deli.each do |person|
+      new_deli << "#{(deli.index(person).to_i + 1)}. #{person}"
+    end
+    puts "The line is currently: #{new_deli.join(" ")}"
+  end
+end
+
+def take_a_number(deli,name)
+  deli << name
+  puts "Welcome, #{name}. You are number #{deli.index(name) + 1} in line."
+end
+
+def now_serving(deli)
+  if deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli[0]}."
+    deli.shift
+  end
+end
