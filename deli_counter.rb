1495fce1ac485966ba788f44bbd79ed83f699aea
Jason Hundal <jasonhundal23@gmail.com>
2019-08-20 21:48:56 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def line(katz_deli)
+puts "what is your name"
+katz_deli = [gets.chomp()]
+      
+      if katz_deli == ""
+          
+          puts "The line is currently empty."
+        else
+            katz_deli.each_with_index do | katz_deli, index |
+        puts "#{katz_deli} are #{index}"
+      end
+end
