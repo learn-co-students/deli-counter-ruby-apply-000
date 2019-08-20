8308cc22d4f6a9623afaea1239093abe860a8c71
Antonnio davis <tonythegoat14@icloud.com>
2019-08-20 20:26:20 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,60 @@
-# Write your code here.
+ ```ruby
+katz_deli = []
+def take_a_number(katz_deli, jason)
+katz_deli << jason
+"Welcome, #{jason}you are number #{katz_deli.1} in line."
+end
+
+def now_serving(katz_deli)
+  
+  katz_del.lenght == 1 
+else
+  "currently serving #{katz_deli.jason}."
+end
+
+end
+
+ def take_a_number(katz_deli, eric)
+   katz_deli << eric
+   def take_a_number(katz_deli, ray)
+     katz_deli ray
+     "welcome, #{eric} and #{ray} you guys are number #{katz_deli.2} and #{katz_deli.3} in line"
+   end
+   
+   def take_a_number(katz_deli, antonio)
+     katz_deli << antonio
+     "welcome, #{antonio} you are number #{katz_deli,4} in line"
+   end
+   
+   def now_serving(katz_deli)
+     katz_del.lenght == 3
+   else
+     "currently serving #{katz_deli.eric}."
+   end
+   
+ end
+ 
+ def now_serving(katz_deli)
+   
+   katz_del.lenght == 2
+ else
+   "currently serving #{katz_deli.ray}."
+  end
+  
+ end
+  
+  def now_serving(katz_deli)
+    
+    katz_del.lenght == 1 
+  else
+    "currently serving #{katz_deli.antonio}."
+  end
+  
+ end
+ 
+ def now_serving(katz_deli)
+   
+   katz_del.lengt == "zero"
+ else
+   "there is nobody to be served"
+   
