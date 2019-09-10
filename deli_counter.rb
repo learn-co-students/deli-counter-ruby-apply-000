540d59ba5894cd3140897c24f914b397078f2e88
schazbot <charlotte.pearce1984@gmail.com>
2019-09-10 13:13:17 +0100

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,29 +1,30 @@
 katz_deli = []
 #line
-def line(line_length)
-	line_array = []
-  	if line_length.length == 0
-  		puts "The line is currently empty."
-	else
-		line_length.collect.with_index do |name, index|
-			line_array.push("#{index + 1}. #{name}")
-		end
-  		puts "The line is currently: #{line_array.join(" ")}"
-	end
+def line(line)
+  line_arr = []
 
-#take_a_number
+  if line.empty?
+    puts "The line is currently empty."
+  else
+    line.each_with_index do |name, index|
+      line_arr << ("#{index + 1}. #{name}")
+    end
+    joined_arr = line_arr.join(" ")
+    puts "The line is currently: #{joined_arr}"
+  end
+end
+
+# #take_a_number
 def take_a_number(katz_deli, name)
-    katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+    katz_deli << name
+	puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
-  if katz_deli.empty?
-then
-    puts "There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{katz_deli[0]}."
-end
-katz_deli.shift
-end
+	if katz_deli.empty?
+		puts "There is nobody waiting to be served!"
+	else 
+		puts "Currently serving #{katz_deli[0]}."
+		katz_deli.shift
+	end
 end
