7ad7db16847658e504ad2d51552ec452b020b857
Serkan Sahin <ssahin55@outlook.com>
2019-11-17 19:10:34 +0000

    Done.

--- README.md
+++ README.md
@@ -31,13 +31,13 @@ Example usage:
   take_a_number(katz_deli, "Grace") #=> Welcome, Grace. You are number 2 in line.
   take_a_number(katz_deli, "Kent") #=> Welcome, Kent. You are number 3 in line.
 
-  line(katz_deli) #=> "The line is currently: 1. Ada 2. Grace 3. Kent"
+  
 
   now_serving(katz_deli) #=> "Currently serving Ada."
 
   line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent"
 
-  take_a_number(katz_deli, "Matz") #=> Welcome, Matz. You are number 3 in line.
+
 
   line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent 3. Matz"
 
--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+katz_deli = [""]
+
+def line(queue_of_people)
+  if queue_of_people.empty?
+    puts "The line is currently empty."
+  else
+    message = "The line is currently:"
+    queue_of_people.each_with_index do |name, index|
+      message = message + " #{index +1}. #{name}"
+    end
+    puts message
+  end
+end
+
+
+def take_a_number(queue_of_people, name_at_end)
+  position = queue_of_people.length + 1
+  message = "Welcome, #{name_at_end}. You are number #{position} in line."
+  queue_of_people << name_at_end
+  puts message 
+end
+
+
+def now_serving (queue_of_people)
+  if queue_of_people.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    first_person_name = queue_of_people.shift
+    message = "Currently serving #{first_person_name}."
+    puts message 
+  end
+end
