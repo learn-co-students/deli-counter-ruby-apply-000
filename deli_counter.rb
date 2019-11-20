acb7923de4ffa0d6fda82dd67161f37e2f0b2a41
Dell Wilson <warren.wilsonjr89@gmail.com>
2019-11-20 22:08:57 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,18 +1,18 @@
 # Write your code here.
-
+ new_deli = []
+ 
+ 
 def line(deli)
   if deli.length < 1 
     puts "The line is currently empty." 
   else 
     i = 0 
     current_line = 'The line is currently:'
-    # print deli.length 
     while i < deli.length 
       n = i + 1
       current_line << " " << (n.to_s) << ". " <<  deli[i] 
       i += 1 
     end 
-    # puts cur_line.inspect
     puts current_line
   end 
 end
@@ -20,11 +20,16 @@ end
 
  
  
-def take_a_number(deli, name) 
-  if deli.length < 0 
-    i = 0
-    n = i + 1 
-    puts "Welcome, #{name}. " << "You are " <<  (n.to_s) << "in line"
-  end 
+def take_a_number(new_deli, name) 
+  new_deli << name 
+  puts "Welcome, #{name}. You are number #{new_deli.length} in line."
 end 
   
+  
+  def now_serving(new_deli)
+    if new_deli.length < 1 
+      puts "There is nobody waiting to be served!"
+    else 
+      puts "Currently serving #{new_deli.shift}."
+    end 
+  end 
