6bb47214379645c398085ce551b99e3e44a4a565
Felix Rodriguez <frod25@gmail.com>
2020-02-12 16:13:49 +0000

    Done.

--- deli_counter_backup.rb
+++ deli_counter_backup.rb
@@ -1,5 +1,17 @@
 katz_deli = []
 
+empty_line = []
+test_line = ["Logan", "Avi", "Spencer"]
+other_line = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+
+def create_indexed_line(line)
+  temp_array = []
+  line.each_with_index do |name, index|
+    temp_array[index] = "#{index+=1}. #{name}"
+  end
+  temp_array
+end
+
 def line(line_array)
   if line_array == []
     puts "The line is currently empty."
@@ -21,14 +33,5 @@ def take_a_number(current_line, name)
     current_line.push("#{name}")
     position = current_line.find_index(name) + 1
     puts "Welcome, #{name}. You are number #{position} in line."
-  end
-end
-
-def now_serving(deli_line)
-  if deli_line == []
-    puts "There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{deli_line[0]}."
-    deli_line.shift
   end
 end
--- deli_counter_test.rb
+++ deli_counter_test.rb
@@ -1,14 +1,23 @@
 katz_deli = []
 
+empty_line = []
+test_line = ["Logan", "Avi", "Spencer"]
+other_line = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+
+def create_indexed_line(line)
+  temp_array = []
+  line.each_with_index do |name, index|
+    temp_array[index] = "#{index+=1}. #{name}"
+  end
+  temp_array
+end
+
 def line(line_array)
   if line_array == []
     puts "The line is currently empty."
   else
-    temp_array = []
-    line_array.each_with_index do |name, index|
-      temp_array[index] = "#{index+=1}. #{name}"
-    end
-    puts "#{ordered_line = "The line is currently: #{temp_array.join(" ")}"}"
+    ordered_line = create_indexed_line(line_array)
+    puts "#{ordered_line = "The line is currently: #{ordered_line.join(" ")}"}"
     ordered_line
   end
 end
