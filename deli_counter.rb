75760e306f0999a79a6310e5709f4e26804232c4
Leonora Squires <leonora.squires@gmail.com>
2020-02-20 17:30:54 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+def line(lineArr)
+  counter = 0
+  lineVar = "The line is currently:"
+  if lineArr == []
+    puts "The line is currently empty."
+  else
+    lineArr.each do |customer|
+      counter+=1
+      lineVar << " #{counter}. #{customer}"
+    end
+    puts lineVar
+  end
+end
+
+def take_a_number(lineArr, newCustomer)
+  lineArr << newCustomer
+  puts "Welcome, #{newCustomer}. You are number #{lineArr.rindex(newCustomer) + 1} in line."
+end
+
+def now_serving(lineArr)
+  if lineArr == []
+    puts "There is nobody waiting to be served!"
+  else
+    #.shift returns first element and removes it from array
+    puts "Currently serving #{lineArr.shift}."
+  end
+end
