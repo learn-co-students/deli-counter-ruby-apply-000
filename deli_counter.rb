093524f7f37546e8330c95bd6cba47b1c2b36344
Darren Koomson <darrenkoomson17@gmail.com>
2020-02-26 17:34:07 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,15 @@
 # Write your code here.
 katz_deli = []
+
 def line(katz_deli)
-  if katz_deli.length > 0
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else katz_deli.length > 0
     sent = "The line is currently:"
     katz_deli.each_with_index do |name, idx|
-      sent <<  " #{idx + 1}. #{name}"
+      sent << " #{idx + 1}. #{name}"
     end
     puts sent
-  else katz_deli.length == 0
-      puts "The line is currently empty."
   end
 end
 
@@ -24,7 +25,5 @@ def now_serving(katz_deli)
   else katz_deli.length > 0
     puts "Currently serving #{katz_deli.first}."
     katz_deli.shift
-
   end
-
 end
