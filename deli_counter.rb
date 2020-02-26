e588beb3b1d12463db2f2534a2a8a9897f779098
Marlon Braga <rdg.brg@gmail.com>
2020-02-26 01:53:21 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def line(arr=[])
+  deliLine = "The line is currently: "
+  if arr.length() === 0
+    puts "The line is currently empty."
+  else
+    arr.each_with_index do |names, index|
+      deliLine += "#{index+1}. #{names}"
+      if index < arr.length()-1
+        deliLine += " "
+      end
+    end
+    puts deliLine
+  end
+end
+
+def take_a_number(line, newPerson)
+  line.push(newPerson)
+  puts "Welcome, #{line[line.length-1]}. You are number #{line.length} in line."
+end
+
+def now_serving(deliLine=[]) 
+  if deliLine.length() === 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deliLine.shift()}."
+  end
+end
