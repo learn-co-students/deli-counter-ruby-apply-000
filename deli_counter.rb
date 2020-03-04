6bb0e0994e8ce7bfbac7ed81b5f425e706b90ab1
interpreted-keyword-9673 <mel.cantorias@gmail.com>
2020-03-04 21:39:18 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,24 +1,21 @@
 # Write your code here.
-katz_deli = []
 def line(array)
-  if array.length >= 1
+  if array.length > 0
     new_array = []
-    counter = 1 
-    array.each do |name|
-      new_array.push("#{counter}. #{name}")
-      counter += 1 
-    end 
+    array.each_with_index { |name, index|
+      new_array << "#{index + 1}. #{name}" }
     puts "The line is currently: #{new_array.join(" ")}"
   else
     puts "The line is currently empty."
   end
 end
 
+katz_deli = []
 line(katz_deli)
 
-def take_a_number(line, customer)
-  line.push(customer)
-  puts "Welcome, #{customer}. You are number #{line.length} in line."
+def take_a_number(array_temp, customer)
+  array_temp << customer
+  puts "Welcome, #{customer}. You are number #{array_temp.length} in line."
 end
 
 take_a_number(katz_deli, "Ada")
@@ -26,12 +23,11 @@ take_a_number(katz_deli, "Grace")
 take_a_number(katz_deli, "Kent")
 line(katz_deli)
 
-def now_serving(line)
-  if line.length == 0
-    puts "There is nobody waiting to be served!"
+def now_serving(array_update)
+  if array_update != []
+    puts "Currently serving #{array_update.shift}."
   else
-    current = line.shift
-    puts "Currently serving #{current}."
+    puts "There is nobody waiting to be served!"
   end
 end
 
