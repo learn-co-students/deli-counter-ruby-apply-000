1f1475f56755357674971a33fe046fd65110196f
Avni Alp Mumcu <a.alp.mumcu@gmail.com>
2020-03-05 20:32:19 +0000

    Done.

--- deli.rb
+++ deli.rb
@@ -1,21 +1,23 @@
+
 katz_deli = []
 
 def line(katz_deli)
-  current_lines = [];
+  line_positions = [];
   if katz_deli.empty?
     puts "The line is currently empty."
   else
     number = 1
     katz_deli.each do |customer|
-      current_lines << "#{number}. #{customer}"
+      line_positions << "#{number}. #{customer}"
       number += 1
     end
-    puts "The line is currently: #{current_lines.join(" ")}"
+    puts "The line is currently: #{line_positions.join(" ")}"
   end
 end
-def take_a_number(katz_deli, person)
-  katz_deli << customer
-  puts "Welcome, #{customer}. You are number #{katz_deli.size} in line."
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
 def now_serving(katz_deli)
