9186369f7ffd6b569eaff6e756469d34a04410da
Zeynab Jibreel <zeina2025@gmail.com>
2020-03-08 20:06:36 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line (katz_deli)
   if katz_deli.length == 0
   puts ("The line is currently empty.")
   else
-  katz_deli.each.with_index (1) do |customer, index|
+  katz_deli.each.with_index  (+1) do |customer, index|
   line.push ("#{index}. #{customer}")
   end
 
