e9f0cf49fe559a14fb4e506da3d14b8f96d3b0e3
Steven Williams <evilgeniusnyc@me.com>
2020-03-09 14:00:25 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,19 +6,17 @@ def line(array)
     puts "The line is currently empty."
   else
     message = "The line is currently:"
-array.each_with_index do |value, index|
-    message += " #{index.to_i+1}. #{value}"
-  end
-  puts "#{message}"
+    array.each_with_index do |value, index|
+      message += " #{index.to_i+1}. #{value}"
+    end
+    puts "#{message}"
   end
 end
 
 
 def take_a_number(array, name)
   array.push(name)
-  position = array.index(name)
   puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
-  return name, position
 end
 
 
