123bca9cc97cca3f997097af8bd6679024e2981b
Steve C <stephen.cases@gmail.com>
2020-03-11 12:54:52 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,32 +1,31 @@
-# Write your code here.
-katz_deli = [] #set to an empty array - no customers
+katz_deli = []
 
 def line(katz_deli)
-  position = [] #create a new array that's empty
-  if katz_deli.empty? #if the katz_deli array is empty (true statement) then
+  position = []
+  if katz_deli.empty?
     puts "The line is currently empty."
   else
-    counter = 1 #start a counter at the first position
-    katz_deli.each do |customer| #iterate each element of katz_deli with |customer|
-      position << "#{counter}. #{customer}" #shovel the counter number and customer name into the position array
-      counter += 1 #increase the counter by 1
+    counter = 1
+    katz_deli.each do |customer|
+      position << "#{counter}. #{customer}"
+      counter += 1
     end
-    puts "The line is currently: #{position.join(" ")}" #interpolate with the position array and use join to convert into an string delimited by an empty space
+    puts "The line is currently: #{position.join(" ")}"
   end
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli.push(name) #adds to the end of the katz_deli array
-  counter = katz_deli.size #set counter to the number or elements are in the array
+  katz_deli.push(name)
+  counter = katz_deli.size
   puts "Welcome, #{name}. You are number #{counter} in line."
-  counter += 1 #increase the counter by 1
+  counter += 1
 end
 
 def now_serving(katz_deli)
-  if katz_deli.empty? #if katz_deli is empty then
+  if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    serving = katz_deli.shift #shift will pull the first element of the katz_deli array and set it to the variable serving
+    serving = katz_deli.shift
     puts "Currently serving #{serving}."
   end
 end
