456f28cb325ecf7ebc94be574db68e79cd97177c
Steve C <stephen.cases@gmail.com>
2020-03-11 16:56:21 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,12 @@
 katz_deli = []
 
-def line(katz_deli)
+def line(deli_array)
   position = []
-  if katz_deli.empty?
+  if deli_array.empty?
     puts "The line is currently empty."
   else
     counter = 1
-    katz_deli.each do |customer|
+    deli_array.each do |customer|
       position << "#{counter}. #{customer}"
       counter += 1
     end
@@ -14,18 +14,18 @@ def line(katz_deli)
   end
 end
 
-def take_a_number(katz_deli, name)
-  katz_deli.push(name)
-  counter = katz_deli.size
+def take_a_number(deli_array, name)
+  deli_array.push(name)
+  counter = deli_array.size
   puts "Welcome, #{name}. You are number #{counter} in line."
   counter += 1
 end
 
-def now_serving(katz_deli)
-  if katz_deli.empty?
+def now_serving(deli_array)
+  if deli_array.empty?
     puts "There is nobody waiting to be served!"
   else
-    serving = katz_deli.shift
+    serving = deli_array.shift
     puts "Currently serving #{serving}."
   end
 end
