4a8e0e263d6a2b2e36f72bd08860aaa08ebf5a59
Ladeeluc <laura.hatch.lh@gmail.com>
2020-03-12 20:41:04 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,38 +1,35 @@
-require 'pry'
-def line(foo)
-        if foo.empty?
+def line(position)
+index = 0
+    if position.empty?# should say the line is empty
         puts "The line is currently empty."
+    else #should display the current line
+        #works with different people in line
+        string = "The line is currently:" 
+        position.each_with_index do |item, index|
+        string << " #{index+1}. #{item}" 
+        end 
+        puts string 
+    end 
+end
 
-        else
-            string = "The line is currently:"
-            foo.each.with_index(1) do|name, index|
-            string << " #{index}. #{name}" 
-            
-            #binding.pry 
-            end 
-            puts string
-        end
-        
+#     should add a person to the line
+#   there are already people in line
+#     should add a person to the end of the line
+#   adding multiple people in a row
+def take_a_number(katz_deli,name)# there is nobody in line
+        katz_deli.push(name)
+        puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
+end
+#should say that the line is empty
+# should serve the first person in line and remove them from the queue
+def now_serving(katz_deli)
+    if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+    else puts "Currently serving #{katz_deli[0]}."
+        katz_deli.shift
     end
-
-
-def take_a_number(array,name)
-        array << "#{name}"
-        puts "Welcome, #{name}. You are number #{array.count} in line."      
 end
 
-def now_serving(deli_array)
-        #deli_array.shift
-       if deli_array.empty? 
-        puts "There is nobody waiting to be served!"
-       else 
-        puts "Currently serving #{deli_array[0]}."
-        deli_array.shift
-
-       end
-end
-#deli_array.shift
-#psuedo code
-#puts statment for string
-#enumerable to remove person from front of array
-# if empty array puts out string# Write your code here.
+#binding.pry
+# katz_deli = []
+# foo = "Ada"
