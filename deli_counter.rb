a70b0e1157b08c748f0f0d371f661522e1367b4f
Rob Betz <volldammd@gmail.com>
2020-03-16 12:47:06 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
-#katz_deli = [] # defines an empty array that represents the empty queue at Katz Deli before it opens (no customers yet.) Arrays are one of the most commonly used data structures in Ruby
-                katz_deli = Array.new # would also work in place of katz_deli = []
+katz_deli = [] # defines an empty array that represents the empty queue at Katz Deli before it opens (no customers yet.) Arrays are one of the most commonly used data structures in Ruby
+               # katz_deli = Array.new would also work to define an empty array in place of katz_deli = []
 
-def take_a_number(array, name)
-  array.push(name)
-  position = array.index(name)
-  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+def take_a_number(array, name) #defines the take_a_number method using an array's coorindnates with an assosicated sequence of names.
+  array.push(name) # .push takes the names and adds them to end of the array, pushing forward the next person in line.
+  position = array.index(name) # .index takes the names' positions within the array to to create a queue of people waiting in line. Ascribing the names to position within the array.
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line." #"puts" sends a message with the name of the ticket taker and their ticket number, which is their place within the array and adds 1 (Ruby array indexing starts at 0.) Also, an index of -1 indicates the last element of the array, -2 is the next to last element in the array, and so on.
   return name, position
 end
 
