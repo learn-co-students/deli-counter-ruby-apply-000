615aadfb14efaa341797949325c525c64be9bd66
effective-cyborg-8832 <jorgeguilln@gmail.com>
2020-03-21 23:05:55 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,14 @@
-# Write your code here.
 def line(katz_deli)
-if katz_deli.empty?
+  if katz_deli.empty?
   puts "The line is currently empty."
 else
-  lines_string = "The line is currently:"
-    katz_deli.each_with_index do |name, i|
-      lines_string << " #{i + 1}. #{name}"
-      end
-      puts lines_string
-    end
-   end
+  current_line = "The line is currently:"
+  katz_deli.each_with_index do |name, i|
+    current_line << " #{i + 1}. #{name}"
+  end
+  puts current_line
+end
+end
 
 def take_a_number(katz_deli, person)
   katz_deli << person
