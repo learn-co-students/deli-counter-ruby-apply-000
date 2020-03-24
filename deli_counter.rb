a92d07fa4c37a01c73f6a465807ce45dfff3025f
Netaly Ramirez <21netaly@gmail.com>
2020-03-24 15:54:50 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,30 +1,28 @@
 # Write your code here.
 
-def line(currentLine)
-
-  if currentLine.length == 0
+def line(arr)
+  if arr.length == 0
     puts "The line is currently empty."
   else
     response = "The line is currently:"
     count = 1
-    currentLine.each do |currentPerson|
-      response+= " #{count}. #{currentPerson}"
-      count+=1
+    arr.each do |currentPerson|
+      response += " #{count}. #{currentPerson}"
+      count +=1
     end
     puts response
   end
 end
 
-def take_a_number(currentLine, name)
-  currentLine.push(name)
-  puts "Welcome, #{name}. You are number #{currentLine.length} in line."
+def take_a_number(current_line, name)
+  current_line << name
+  puts "Welcome, #{name}. You are number #{current_line.length} in line."
 end
 
-def now_serving(currentLine)
-  if currentLine.length == 0
+def now_serving(current_line)
+  if current_line.length == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{currentLine[0]}."
-    currentLine.shift()
+    puts "Currently serving #{current_line.shift()}."
   end
 end
