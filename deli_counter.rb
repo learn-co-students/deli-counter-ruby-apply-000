dd6a32414ef24d64f8625a97b6eae22390ad2e28
Duke Ko <dukhyunko93@gmail.com>
2020-03-29 20:06:53 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,20 +1,19 @@
 katz_deli = [ ]
 
-def take_a_number(katz_deli,name)
-  katz_deli.push(name)
-  position = katz_deli.index(name)
-  puts "Welcome, #{name}. You are number #{position+1} in line."
-end
-
 def line(katz_deli)
-  if katz_deli.length == 0
+  if katz_deli == [ ]
     puts "The line is currently empty."
   else message = "The line is currently:"
-    katz_deli.each_with_index do |name,position|
-      message += " #{position.to_i+1}. #{name}"
+    katz_deli.each_with_index do |name,index|
+      message += " #{index + 1}. #{name}"
     end
     puts message
   end
+end
+
+def take_a_number(katz_deli,name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
 end
 
 def now_serving(katz_deli)
