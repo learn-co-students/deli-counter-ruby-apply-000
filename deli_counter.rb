43eeff5d7da4d3a1f4d504270f639e352ee6110d
Duke Ko <dukhyunko93@gmail.com>
2020-03-30 13:38:46 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,3 @@
-katz_deli = [ ]
-
 def line(katz_deli)
   if katz_deli == [ ]
     puts "The line is currently empty."
@@ -13,7 +11,7 @@ end
 
 def take_a_number(katz_deli,name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
