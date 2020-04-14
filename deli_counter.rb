c9105cd45a3c549b48bd6e1d48277379638a376e
Shadman Ahmed <shadman@outlook.com>
2020-04-14 08:23:42 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,19 +1,15 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli.length == 0
+  if katz_deli.empty? == true
     puts "The line is currently empty."
   else
     message="The line is currently:"
-
     katz_deli.each_with_index do |value, index|
       message += " #{index.to_i+1}. #{value}"
     end
-
     puts "#{message}"
   end
-
-
 end
 
 
