a60aa8810d1a1713f927f4486419f1a8d677f91a
Shadman Ahmed <shadman@outlook.com>
2020-04-15 06:26:33 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
   else
     message="The line is currently:"
     katz_deli.each_with_index do |value, index|
-      message += " #{index.to_i+1}. #{value}"
+      message += " #{index+1}. #{value}"
     end
     puts "#{message}"
   end
