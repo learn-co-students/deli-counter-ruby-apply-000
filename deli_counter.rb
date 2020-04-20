64a7396585503ff40de11f99b6c745399c6fd9a7
Maysoon Tabali <mtab03@me.com>
2020-04-20 05:20:22 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,26 +1,58 @@
-# Write your code here.
-katz_deli = []
 
+#Start of day the deli is empty so the queue is represented by an empty array counter # Write your code here.
+#katz_deli = []
+
+
+#line method shows everyone their current place in the line
+#def line(katz_deli)
+#otherwise run this code if condition is not met
+#else
+  #number = 1
+  #katz_deli.each do |customer|
+    #line_positions << "#{number}. #{customer}"
+    #number += 1
+  #end
+  #puts "The line is currently: #{line_positions.join(" ")}"
+  #end
+#end
+
+#def take_a_number(katz_deli, name)
+    #katz_deli << name
+  #  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+#end
+#end
+
+#the deli is empty so the queue is represented by an empty array
+katz_deli = []
+#line method shows everyone their current place in the line
 def line(katz_deli)
-  line_positions = [];
+  line_positions = []
+  #if conditon is met (true) and string length is zero or empty
   if katz_deli.empty?
-  puts "The line is currently empty."
-else
-  number = 1
-  katz_deli.each do |customer|
-    line_positions << "#{number}. #{customer}"
-    number += 1
-  end
-  puts "The line is currently: #{line_positions.join(" ")}"
+    #run this code if condition is met
+    puts "The line is currently empty."
+  #otherwise run this code if condition is not met (false)
+  else
+    number = 1
+    katz_deli.each do |customer|
+      line_positions << "#{number}. #{customer}"
+      number += 1
+    end
+    puts "The line is currently: #{line_positions.join(" ")}"
   end
 end
 
-  take_a_number(katz_deli, "name")
-    puts "Welcome, Ada. You are number 1 in line."
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end
 
-  take_a_number(katz_deli, "Grace")
-      puts "Welcome, Grace. You are number 2 in line."
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
 
-  take_a_number(katz_deli, "Kent")
-        puts "Welcome, Kent. You are number 3 in line."
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
 end
