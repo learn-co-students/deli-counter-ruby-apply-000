24d86a89505fca3316f823f1298f1d6075631764
Maysoon Tabali <mtab03@me.com>
2020-04-21 22:10:36 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,46 +1,34 @@
-#Start of day the deli is empty so the queue is represented by an empty array 
+#Start of day the deli is empty so the queue is represented by an empty array
+#functions as outside varible with empty array
 katz_deli = []
-#line method shows everyone their current place in the line
+#function calling line method that accepts argument
 def line(katz_deli)
+  #function stores numerous items or elements from below code
   line_positions = []
-  #if conditon is met (true) and string length is zero or empty
+  #function if conditon is met (true) print puts
   if katz_deli.empty?
-    #run this code if condition is met
     puts "The line is currently empty."
-  #otherwise run this code if condition is not met (false)
+  #function prints number and customer into string and increments it by 1
   else
-    #start the line count at 1
     number = 1
-    #invoke the each iterater block on katz_deli and pass an argument on each element in the katz_deli array using pipes ||
     katz_deli.each do |customer|
-      #when the block is passed the line position array << adds number and customer to the end of the queue
       line_positions << "#{number}. #{customer}"
-      #line position is incremented by 1 each time the block is passed on katz_deli array
       number += 1
     end
-    #prints out current line using .join which combines number and customer into a string
     puts "The line is currently: #{line_positions.join(" ")}"
   end
 end
-#define take_a_number method with two arguments
+#function adds new cutomers name and number to end of line queue
 def take_a_number(katz_deli, name)
-  #shovels name to end of katz_deli line or array
   katz_deli << name
-  #prints customers name and number in line by using .size method
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
-#define now_serving method with katz_deli array
+#function to call next person in line and then remove them from front or prints empty
 def now_serving(katz_deli)
-  #if condition is met (true) and string length is zero or empty
   if katz_deli.empty?
-    #run or output this code below
     puts "There is nobody waiting to be served!"
-
-  #otherwise we run below code if above condition is not met (false)
   else
-    #outputs currently serving making them .first in the line
     puts "Currently serving #{katz_deli.first}."
-    #then pop or remove them from the front by using .shift
     katz_deli.shift
   end
 end
