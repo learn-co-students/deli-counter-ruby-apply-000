f36c3b68858a26e53d0ec415d2d34c2c7603515d
Sean Xiong <seanxionghe@gmail.com>
2020-04-26 08:33:01 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 
 def line(katz_deli)
-  if katz_deli.empty?
+  if katz_deli.size == 0
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
