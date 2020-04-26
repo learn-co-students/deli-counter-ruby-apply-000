c6e2414aec469429a0cc864e15d580a8429e5c33
Sean Xiong <seanxionghe@gmail.com>
2020-04-26 18:01:22 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  if katz_deli.empty?
+  if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli[0]}."
