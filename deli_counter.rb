5858be672a01cfc1ce97d8bad7c05bfaeed3beb7
Shadman Ahmed <shadman@outlook.com>
2020-05-01 13:42:41 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,14 +4,13 @@ def line(katz_deli)
   if katz_deli.empty? == true
     puts "The line is currently empty."
   else
-    message="The line is currently:"
-    katz_deli.each_with_index do |value, index|
-      message += " #{index+1}. #{value}"
+    message = "The line is currently:"
+    katz_deli.each_with_index do |name, index|
+      message += " #{index+1}. #{name}"
     end
     puts "#{message}"
   end
 end
-
 
 def take_a_number (katz_deli, new_name)
   katz_deli << new_name
