97b963678fd85908904ae0a0f9fd1544a0b6d4b5
bright-suite-6513 <huang.stan@icloud.com>
2020-05-10 23:23:37 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,25 +1,19 @@
 # Write your code here.
 def line(katz_deli)
-  i = 0
   str = "The line is currently:"
   if katz_deli == []
     puts "The line is currently empty."
   else
-    while i < katz_deli.length
-      str << " #{i+1}. #{katz_deli[i]}"
-      i += 1
+    katz_deli.each_with_index do |name, index|
+      str << " #{index+1}. #{name}"
     end
     puts str
   end
 end
 
 def take_a_number(katz_deli, name)
-  if katz_deli == []
     katz_deli << name
-  else
-    katz_deli << name
-  end
-  puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
+    puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
 end
 
 def now_serving(katz_deli)
