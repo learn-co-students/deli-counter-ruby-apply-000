203e87c452e56cd2290e19811874b1d1f0a0c25d
fearless-command-2358 <iuriseara@gmail.com>
2020-05-19 16:39:17 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,19 +1,19 @@
 #Write your code here.
 def line(katz_deli)
-  if katz_deli.length == 0
+  if katz_deli.empty?
     puts "The line is currently empty."
   else
     message="The line is currently:"
 
-    katz_deli.each_with_index do |value, index|
-      message += " #{index.to_i+1}. #{value}"
+    katz_deli.each_with_index do |name, index|
+      message << " #{index + 1}. #{name}"
     end
 
     puts "#{message}"
   end
 
 def now_serving(katz_deli)
-  if katz_deli.length == 0
+  if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli.shift}."
