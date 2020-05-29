46ec9cbcb5e0bcef59614ef380636adef44fcef1
resourceful-composition-1273 <cozycosmic@gmail.com>
2020-05-29 23:49:54 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,25 +1,22 @@
-  katz_deli = ( ) 
- 
- take_a_number .katz_deli,"John". #=> Welcome, John. You are number 1 in line.
- take_a_number .katz_deli,"Jill". #=> Welcome, Jill. You are number 2 in line.
- take_a_number .katz_deli,"Michelle". #=> Welcome, Michelle. You are number 3 in line.
-  
-  line .katz_deli. #=> "The line is currently. 1. John 2. Jill 3. Michelle"
-  
-  now_serving .katz_deli. #=> "Currently serving John."
-  
-  line .katz_deli. #=> "The line is currently. 1. Jill 2. Michelle"
-  
-  take_a_number .katz_deli,"Stephanie". #=> Welcome, Stephanie. You are number 3 in line.
-  
-  line .katz_deli. #=> "The line is currently. 1. Jill 2. Michelle 3. Stephanie "
-  
-  now_serving.katz_deli. #=> "Currently serving Jill."
-  
-  line .katz_deli. #=> "The line is currently. 1. Michelle 2. Stephanie "
-  
-  now_serving.katz_deli. #=> "Currently serving Michelle."
-  
-  now_serving.katz_deli. #=> "Currently serving Stephanie."
-  
-  katz_deli = ( )
+#line "the line is currently empty."
+
+#method name    #argument
+def take_a_number(katz_deli,name) #=>
+katz_deli.push(name) #=>
+puts ("Welcome, #{name}. You are number #{katz_deli.length} in line.")
+end
+
+#line "the line is currently empty."
+
+#method name    #argument
+def now_serving(katz_deli) #=>
+katz_deli.push(name) #=>
+puts ("Currently serving #{name}.")
+end
+
+#method name    #argument
+def line(katz_deli)
+if line()
+else "The line is currently empty."
+puts katz_deli.length(name) #=>
+end
