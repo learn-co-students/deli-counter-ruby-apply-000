85a76deda9e11e3182dcb4ec7248aeb24f9911f2
John Chen <chenj317@gmail.com>
2020-07-16 19:58:37 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,46 @@
 # Write your code here.
+
+katz_deli = []
+other_line = ["Logan", "Avi", "Spencer"]
+another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"] 
+
+def line (customers)
+  
+  #if line if empty dipslay "The line is currently empty"
+  if customers.length == 0 
+    puts "The line is currently empty."
+  
+  #if there are customers display their position and name
+  
+  elsif customers.length > 0
+          phrase = "The line is currently:"
+          customers.each_with_index do |name, index|
+          phrase += " #{index + 1}. #{name}"
+        end
+      puts phrase
+    end
+  end
+  
+#--------------------------------------------------------- 
+
+def take_a_number (customers, name)
+  customers.push(name)
+  puts "Welcome, #{name}. You are number #{customers.index(name) + 1} in line."
+end
+
+#--------------------------------------------------------------
+
+def now_serving(customers)
+      if customers.length == 0
+        puts "There is nobody waiting to be served!"
+      elsif customers.length > 0
+    	 puts "Currently serving #{customers[0]}."
+      end
+    customers.shift
+end
+
+take_a_number(katz_deli, "Ada")
+take_a_number(katz_deli, "Bob")
+line(katz_deli)
+now_serving(katz_deli) 
+line(katz_deli)
