807dff5704b2c827aba357de04caa0928e92c722
John Chen <chenj317@gmail.com>
2020-07-16 21:06:05 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,13 +2,13 @@
 
 katz_deli = []
 
-def line (customers)
-  if customers.length == 0 
+def line (katz_deli)
+  if katz_deli.length == 0 
     puts "The line is currently empty."
   
-  elsif customers.length > 0
+  elsif katz_deli.length > 0
           phrase = "The line is currently:"
-          customers.each_with_index do |name, index|
+          katz_deli.each_with_index do |name, index|
           phrase += " #{index + 1}. #{name}"
         end
       puts phrase
@@ -16,19 +16,19 @@ def line (customers)
   end
 
 
-def take_a_number (customers, name)
-  customers.push(name)
-  puts "Welcome, #{name}. You are number #{customers.index(name) + 1} in line."
+def take_a_number (katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
 end
 
 
-def now_serving(customers)
-      if customers.length == 0
+def now_serving(katz_deli)
+      if katz_deli.length == 0
         puts "There is nobody waiting to be served!"
-      elsif customers.length > 0
-    	 puts "Currently serving #{customers[0]}."
+      elsif katz_deli.length > 0
+    	 puts "Currently serving #{katz_deli[0]}."
       end
-    customers.shift
+    katz_deli.shift
 end
 
 =begin
