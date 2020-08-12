fb5c11e671d7a34578f2a72ed76e565f79ca66f0
friendly-machine-4891 <liamhanafee@gmail.com>
2020-08-12 14:11:38 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,7 +25,7 @@ def line(katz_deli)
   else
     number = 1
     katz_deli.each do |customer|
-      place_on_line << "#{number}. #{customer}"
+      place_on_line.push "#{number}. #{customer}"
       number += 1
     end
     puts "The line is currently: #{place_on_line.join(" ")}"
