b0af08083075a148dbe2993cc2da027b643f0832
Anshul Arora <a.99.anshul@gmail.com>
2020-09-04 21:28:00 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,52 @@
-# Write your code here.
+katz_deli = []
+$current_line = []
+# print out the current line, if there is no line, print empty line
+def line(arr)
+  if arr.empty?
+    puts "The line is currently empty." 
+  else
+    $current_line.clear
+    line_helper(arr)
+    puts "The line is currently:#{$current_line.join}"
+  end
+end
+
+
+def line_helper(arr)
+  arr.each do |name|
+    position = arr.index(name) + 1
+    pos = " " + position.to_s + ". "
+    $current_line.push(pos, name)
+  end
+end
+
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  position = katz_deli.index(name) + 1
+  puts "Welcome, #{name}. You are number #{position} in line."
+end
+
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    first = katz_deli.shift()
+    puts "Currently serving #{first}."
+    # katz_deli.each do |name|
+    #   name = katz_deli.shift()
+    #   puts "Currently serving #{name}."
+    # end
+  end
+end
+
+
+
+
+
+
+
+
+
+
