a2e2e33ad417bbdca82cc5c8b073911d569db418
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-09-08 16:24:13 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,12 +3,14 @@ katz_deli = []
 def line(array)
   katz_deli_line = [] 
   array.map.with_index(1) {|name, index| katz_deli_line << "#{index}. #{name}"}
-  if katz_deli_line == []
-    puts "The line is currently empty."
-  else
-    puts "The line is currently: #{katz_deli_line.join(" ")}"
-  end
+  katz_deli_line == [] ? (puts "The line is currently empty.") : (puts "The line is currently: #{katz_deli_line.join(" ")}")
 end
+#   if katz_deli_line == []
+#     puts "The line is currently empty."
+#   else
+#     puts "The line is currently: #{katz_deli_line.join(" ")}"
+#   end
+# end
 
 def take_a_number(array, name)
   if array
