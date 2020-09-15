9f558b59563c1d787731160071923bcf6f3bbc81
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-15 07:25:35 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,12 +4,12 @@ katz_deli = []
 
 #line Method
 def line(katz_deli)
-  if katz_deli.length == 0
+  if katz_deli.length == 0 #determine the lenth of the array = of the line
     puts "The line is currently empty."
   else
     list = "The line is currently:"
-    katz_deli.each.with_index do |name, index|
-    list += " #{index.to_i+1}. #{name}"
+    katz_deli.each_with_index do |name, index| #allows to iterate over items along with an index keeping count of the item
+    list += " #{index.to_i+1}. #{name}" #Remember that people like to count from 1, not from 0 like computers
     end
     puts "#{list}"
   end
@@ -18,7 +18,7 @@ end
 
 #take_a_number Method
 def take_a_number(katz_deli, name)
-  katz_deli.push(name)
+  katz_deli.push(name) #add person to the END of the line
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
@@ -28,6 +28,6 @@ def now_serving(katz_deli)
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli[0]}."
-    katz_deli.shift
+    katz_deli.shift #remove the FIRST person from the FRONT of the line
   end  
 end
