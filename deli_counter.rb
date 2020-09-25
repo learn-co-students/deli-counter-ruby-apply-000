41ac76815c819ea9c37fd2f8b44191cdaee59173
Oumaima Azzat <oumaimazzat@gmail.com>
2020-09-25 17:27:55 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,5 @@
 katz_deli = [] 
 
-def take_a_number(array, name)
-  array.push(name)
-  position = array.index(name)
-  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
-  return name, position 
-end 
-
-take_a_number(katz_deli, "Ada")
-take_a_number(katz_deli, "Grace")
-take_a_number(katz_deli, "Kent")
-
 def line(array) 
   if array.length == 0 
     puts "The line is currently empty."
@@ -18,13 +7,24 @@ def line(array)
     message = "The line is currently:"
     
   array.each_with_index do |value, index| 
-    message += " #{index.to_i+1}. #{value}"
+    message << " #{index.to_i+1}. #{value}"
   end 
   puts "#{message}"
   end 
 end 
- 
-line(katz_deli)
+
+
+def take_a_number(array, name)
+  array.push(name)
+  position = array.index(name)
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+  return name, position 
+end 
+
+take_a_number(katz_deli, "Ada") #=> Welcome, Ada. You are number 1 in line.
+take_a_number(katz_deli, "Grace") #=> Welcome, Grace. You are number 2 in line.
+take_a_number(katz_deli, "Kent") #=> Welcome, Kent. You are number 3 in line.
+
 
 
 def now_serving(array)
