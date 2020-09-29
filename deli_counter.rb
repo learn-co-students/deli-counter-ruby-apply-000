80dda6bfd82b57f4bf6bedbf5c79cde2309d5475
proactive-researcher-8324 <marymichelle1118@gmail.com>
2020-09-29 23:22:40 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,10 +7,6 @@ def take_a_number(array, name)
   return name, position 
 end 
 
-take_a_number(katz_deli, "Grace")
-take_a_number(katz_deli, "Tom")
-take_a_number(katz_deli, "Alan")
-
 def line(array) 
   if array.length == 0 
     puts "The line is currently empty."
