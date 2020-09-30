bc70067c205174927905fc1b457c01729d0b6e15
Harrison Chung <harrison.y.chung@gmail.com>
2020-09-30 21:12:21 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,12 +6,12 @@ katz_deli = []
 
 #line method
 #I'm defining my line method using the argument "katz_deli."" I'm using an if else conditional statement here to tell my program to make a decision.
-def line(katz_deli)
+ def line(katz_deli)
  length = "The line is currently:"
 #i'm using a string class method (.length) here to calculate the character length of the array "katz_deli."
   if katz_deli.length == 0
    puts "The line is currently empty."
-    else
+ else
 #I'm enumerating the katz_deli array to get the name of the customer and index to calculate the customer's position on line.
        katz_deli.each_with_index do |name, index|
 #I'm using shorthand operator += to signify my variable "length" is equal to variable "length" variable "index+1 + name"
@@ -27,9 +27,9 @@ end
 #using the shovel operator << lets me add the name of customers to the end of the array.
 def take_a_number (katz_deli, name)
    katz_deli << name
-    if katz_deli == 0
+    if katz_deli.length == 0
       puts "The line is currently empty."
-       else
+    else
 # .length returns number of names in katz_deli array.
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
   end
@@ -40,7 +40,7 @@ end
 def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
-      else
+  else
     puts "Currently serving #{katz_deli[0]}."
     katz_deli.shift
   end
