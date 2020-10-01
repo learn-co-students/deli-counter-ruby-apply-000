ae13ac942d0bcee4b6051b7b5d60f42b0990c07c
proactive-researcher-8324 <marymichelle1118@gmail.com>
2020-10-01 15:47:46 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,10 +5,11 @@ def line(array)
     puts "The line is currently empty."
   else 
     message = "The line is currently:"
-  
-  array.each_with_index do |value, index| 
-  message += " #{index +1}. #{value}"
+    
+  array.each_with_index do |name, index| 
+  message += " #{index +1}. #{name}"
   end 
+  
   puts "#{message}"
   end 
 end   
