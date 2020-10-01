c15076b3baeb2680675901389670e4a771fe50d7
proactive-researcher-8324 <marymichelle1118@gmail.com>
2020-10-01 01:15:03 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,5 @@
 katz_deli = [] 
 
-def take_a_number(array, name)
-  array.push(name)
-  position = array.index(name) +1
-  puts "Welcome, #{name}. You are number #{position} in line."
-end 
-
 def line(array) 
   if array.length == 0 
     puts "The line is currently empty."
@@ -17,7 +11,13 @@ def line(array)
   end 
   puts "#{message}"
   end 
-end    
+end   
+
+def take_a_number(array, name)
+  array.push(name)
+  position = array.index(name) +1
+  puts "Welcome, #{name}. You are number #{position} in line."
+end 
 
 def now_serving(array)
   if array.empty? == true 
