191e10b90bc86d48d3e0110591363d66766ef527
Harrison Chung <harrison.y.chung@gmail.com>
2020-10-04 19:49:59 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,13 @@
 #goal of my program is to create a computerized queue to keep track of customers (Ada, Kent, Grace) and improve productivity
-
+#breaking down my code via 3 methods (line method, take_a_number method, now_serving method)
 
 #this is the beginning of the day. Currently there are no customers, so an empty katz's is represented by an empty array.
 katz_deli = []
 
-#line method
+#line method - shows everyone their current place in line
 #I'm defining my line method using the argument "katz_deli."" I'm using an if else conditional statement here to tell my program to make a decision.
  def line(katz_deli)
- length = "The line is currently:"
+ deli_line = "The line is currently:"
 #i'm using a string class method (.length) here to calculate the character length of the array "katz_deli."
   if katz_deli.length == 0
    puts "The line is currently empty."
@@ -15,12 +15,13 @@ katz_deli = []
 #I'm enumerating the katz_deli array to get the name of the customer and index to calculate the customer's position on line.
        katz_deli.each_with_index do |name, index|
 #I'm using shorthand operator += to signify my variable "length" is equal to variable "length" variable "index+1 + name"
-#Additionally, I'm interpolating my variables below to make my code clean, and legible.
-      length += " #{index+1}. #{name}"
+#Additionally, I'm interpolating my variable "length" below to make my code clean, and legible.
+      deli_line += " #{index+1}. #{name}"
     end
-    puts "#{length}"
+    puts "#{deli_line}"
   end
 end
+
 
 #Next, I'm building a new method that a new customer will use when entering the deli.
 #defining our take_a_number method with 2 arguments (katz_deli, name) Name signifies the name of the custoemr. In this Program would be Ada, Grace, Kent
@@ -34,6 +35,7 @@ def take_a_number (katz_deli, name)
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
   end
 end
+
 
 #Finally, I'm building the now_serving method which should call out the next person in line and then remove them from the front of the line.
 # .shift is an array class method which removes the first element of self, and returns it or nil if the array is empty. In this case, it's removing the first person's name from the array.
