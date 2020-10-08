7aad84fff6a48bc9d9d1587ab5a0751ab54e7bbc
Melissa F <melissa.faulner@gmail.com>
2020-10-08 22:14:37 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,10 +20,10 @@ def take_a_number(katz_deli, customer)
 end
 
 def now_serving(katz_deli)
-  if katz_deli.empty? == TRUE
+  if katz_deli.empty?
     puts "There is nobody waiting to be served!"
     
-  elsif katz_deli.empty? == FALSE
+  elsif !(katz_deli.empty?)
     puts "Currently serving #{katz_deli.shift}."
   end
 end
